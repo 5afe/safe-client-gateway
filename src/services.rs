8e62af1c4d86923087463c1601fde381489bf4f9
Jose Pablo Alvarez Lacasia <jose.alvarez@gnosis.io>
2020-07-16 10:19:37 +0200

    Made the formatting of the URL more compact

--- src/services.rs
+++ src/services.rs
@@ -1,6 +1,5 @@
 pub mod transactions;
 
 fn base_transaction_service_url() -> String {
-    let base = dotenv!("TRANSACTION_SERVICE_URL").to_string();
-    format!("{}{}", base, "/api/v1")
+    format!("{}{}", dotenv!("TRANSACTION_SERVICE_URL"), "/api/v1")
 }

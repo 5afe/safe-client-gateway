b849bade2161d8b18bc0f97c079cbac72df43059
Jose Pablo Alvarez Lacasia <jose.alvarez@gnosis.io>
2020-07-16 10:17:16 +0200

    Added dotenv crate and used it for transaction service base URL

--- src/services.rs
+++ src/services.rs
@@ -1,8 +1,6 @@
 pub mod transactions;
 
-use std::env;
-
 fn base_transaction_service_url() -> String {
-    let transactions_service_host = env::var("TRANSACTION_SERVICE_URL").expect("Must have TRANSACTION_SERVICE_URL with host defined");
-    format!("{}{}", transactions_service_host, "/api/v1")
+    let base = dotenv!("TRANSACTION_SERVICE_URL").to_string();
+    format!("{}{}", base, "/api/v1")
 }

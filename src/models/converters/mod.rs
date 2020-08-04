e12b12df8707b54df754aab86caf50aec3f4ed69
Jose Pablo Alvarez Lacasia <jose.alvarez@gnosis.io>
2020-08-04 18:48:24 +0200

    Removed ugliness

--- src/models/converters/mod.rs
+++ src/models/converters/mod.rs
@@ -1,12 +1,14 @@
 pub mod transactions;
-pub mod transfers;use crate::models::service::transactions::TransferDirection;
+pub mod transfers;
+
+use crate::models::service::transactions::TransferDirection;
 
 fn get_transfer_direction(safe: &String, from: &String, to: &String) -> TransferDirection {
-        if safe == from {
-            TransferDirection::Outgoing
-        } else if safe == to {
-            TransferDirection::Incoming
-        } else {
-            TransferDirection::Unknown
-        }
+    if safe == from {
+        TransferDirection::Outgoing
+    } else if safe == to {
+        TransferDirection::Incoming
+    } else {
+        TransferDirection::Unknown
+    }
 }

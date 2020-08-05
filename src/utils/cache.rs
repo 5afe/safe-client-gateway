f8f8c527bfa2fa95130d4178fb7411af78c226e5
Richard Meissner <rmeissner@users.noreply.github.com>
2020-08-05 14:57:35 +0200

    Closes #40: Cache invalidation via webhook (#62)

--- src/utils/cache.rs
+++ src/utils/cache.rs
@@ -1,8 +1,8 @@
-use rocket_contrib::databases::redis::{self, Commands};
-use serde_json;
-use serde::ser::{Serialize};
 use anyhow::Result;
 use rocket::response::content;
+use rocket_contrib::databases::redis::{self, pipe, Commands, Iter, PipelineCommands};
+use serde::ser::Serialize;
+use serde_json;
 
 #[database("service_cache")]
 pub struct ServiceCache(redis::Connection);
@@ -19,12 +19,22 @@ impl ServiceCache {
         let _: () = self.set_ex(id, dest, timeout).unwrap();
     }
 
+    pub fn invalidate_pattern(&self, pattern: &String) {
+        pipeline_delete(self, self.scan_match(pattern).unwrap());
+    }
+
     pub fn _invalidate(&self, id: &String) {
         let _: () = self.del(id).unwrap();
     }
 
-    pub fn cache_resp<R>(&self, key: &String, timeout: usize, resp: impl Fn() -> Result<R>) -> Result<content::Json<String>>
-        where R: Serialize
+    pub fn cache_resp<R>(
+        &self,
+        key: &String,
+        timeout: usize,
+        resp: impl Fn() -> Result<R>,
+    ) -> Result<content::Json<String>>
+    where
+        R: Serialize,
     {
         let cached = self.fetch(key);
         match cached {
@@ -38,7 +48,12 @@ impl ServiceCache {
         }
     }
 
-    pub fn request_cached(&self, client: &reqwest::blocking::Client, url: &String, timeout: usize) -> Result<String>  {
+    pub fn request_cached(
+        &self,
+        client: &reqwest::blocking::Client,
+        url: &String,
+        timeout: usize,
+    ) -> Result<String> {
         let data: String = match self.fetch(&url) {
             Some(cached) => cached,
             None => {
@@ -49,4 +64,12 @@ impl ServiceCache {
         };
         Ok(data)
     }
+}
+
+fn pipeline_delete(con: &redis::Connection, keys: Iter<String>) {
+    let pipeline = &mut pipe();
+    for key in keys {
+        pipeline.del(key);
+    }
+    pipeline.execute(con);
 }

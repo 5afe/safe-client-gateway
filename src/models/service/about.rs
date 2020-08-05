373c9b6f4174203451e578188a4817cf16650f65
Jose Pablo Alvarez Lacasia <jose.alvarez@gnosis.io>
2020-08-05 12:56:50 +0200

    Built about struct for about endpoint to contained specified fields in task

--- src/models/service/about.rs
+++ src/models/service/about.rs
@@ -3,6 +3,9 @@ use serde::Serialize;
 #[derive(Serialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct About {
+    pub transaction_service_base_url: String,
     pub name: String,
     pub version: String,
+    pub transaction_service_version: Option<String>,
+    pub build_number: Option<String>,
 }
--- src/services/about.rs
+++ src/services/about.rs
@@ -1,17 +1,30 @@
 extern crate reqwest;
 
 use crate::config::{base_transaction_service_url};
-use crate::models::backend::about::About;
+use crate::models::backend::about::About as AboutDto;
 use anyhow::Result;
 use reqwest::Url;
+use crate::models::service::about::About;
+use std::process::Command;
 
-pub fn get_about() -> Result<String> {
+pub fn get_about() -> Result<About> {
     let url_string = format!("{}{}", base_transaction_service_url(), "/about");
     let url = Url::parse(&url_string)?;
     let body = reqwest::blocking::get(url)?.text()?;
-    let about: About = serde_json::from_str(&body)?;
-    Ok(format!(
-        "This is an API wrapper for {}, version {}\nNo guarantees in terms of availability.",
-        about.name, about.api_version
-    ))
+    let about_dto: AboutDto = serde_json::from_str(&body)?;
+
+    let command = Command::new("git")
+        .arg("rev-list")
+        .arg("--count")
+        .arg("master")
+        .output()?;
+    let build_number = std::str::from_utf8(command.stdout.as_slice())?.trim();
+
+    Ok(About {
+        transaction_service_base_url: base_transaction_service_url(),
+        name: env!("CARGO_PKG_NAME").to_string(),
+        version: env!("CARGO_PKG_VERSION").to_string(),
+        transaction_service_version: Some(about_dto.version),
+        build_number: Some(build_number.to_string()),
+    })
 }

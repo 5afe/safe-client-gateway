17dcd40bea69fde2f50f5f6cfb2ef7ed8750a9b9
Jose Alvarez <jpalvarezl@users.noreply.github.com>
2020-08-07 17:17:13 +0200

    Closes #74 Safe TX hash usable for TX Details endpoint (#83)
    * Made multisig details as the else branch for route resolution
    
    * Marginally improved implementation

--- src/services/transactions_details.rs
+++ src/services/transactions_details.rs
@@ -130,6 +130,7 @@ pub fn get_transactions_details(
                 .get(3)
                 .ok_or(anyhow::anyhow!("No module tx details hash"))?,
         ),
-        &_ => Err(anyhow::anyhow!("Invalid details type")),
+        &_ => get_multisig_transaction_details(context, tx_type)
+            .map_err(|_| anyhow::anyhow!("Invalid details type or Id")),
     }
 }

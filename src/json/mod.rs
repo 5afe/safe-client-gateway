5dec85a06c4525bf3b4a9c16b51fd8a7845b0960
Jose Pablo Alvarez Lacasia <jose.alvarez@gnosis.io>
2020-08-12 17:12:01 +0200

    Extracted json into a json test module

---
+++ src/json/mod.rs
@@ -0,0 +1,3 @@
+pub const MULTISIG_TX_ERC20_TRANSFER: &str = include_str!("transactions/multisig_erc20_transfer.json");
+pub const SAFE_WITH_MODULES: &str = include_str!("safes/with_modules.json");
+pub const TOKEN_USDT: &str  = include_str!("tokens/USDT.json");

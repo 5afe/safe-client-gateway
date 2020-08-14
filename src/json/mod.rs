d1e771b4e59f36424dfb2b1a36d81d95d1fe0ff0
Jose Pablo Alvarez Lacasia <jose.alvarez@gnosis.io>
2020-08-14 11:47:23 +0200

    Added unit test for details from module transaction

--- src/json/mod.rs
+++ src/json/mod.rs
@@ -3,8 +3,10 @@ pub const MULTISIG_TX_ERC721_TRANSFER: &str = include_str!("transactions/multisi
 pub const MULTISIG_TX_ETHER_TRANSFER: &str = include_str!("transactions/multisig_ether_transfer.json");
 pub const MULTISIG_TX_SETTINGS_CHANGE: &str = include_str!("transactions/multisig_addOwnerWithThreshold_settings_change.json");
 pub const MULTISIG_TX_CUSTOM: &str = include_str!("transactions/multisig_approve_custom.json");
+pub const MODULE_TX: &str = include_str!("transactions/module_transaction.json");
 
 pub const SAFE_WITH_MODULES: &str = include_str!("safes/with_modules.json");
+pub const _SAFE_WITH_MODULE_TXS: &str = include_str!("safes/with_module_transactions.json");
 
-pub const TOKEN_USDT: &str  = include_str!("tokens/USDT.json");
+pub const TOKEN_USDT: &str = include_str!("tokens/USDT.json");
 pub const TOKEN_CRYPTO_KITTIES: &str = include_str!("tokens/crypto_kitties.json");
--- src/models/converters/transactions/tests/details.rs
+++ src/models/converters/transactions/tests/details.rs
@@ -1,13 +1,13 @@
-use crate::models::backend::transactions::MultisigTransaction;
+use crate::models::backend::transactions::{MultisigTransaction, ModuleTransaction};
 use crate::providers::info::SafeInfo;
 use crate::providers::info::*;
-use crate::models::service::transactions::details::{TransactionDetails, TransactionData, MultisigExecutionDetails, DetailedExecutionInfo, MultisigConfirmation};
+use crate::models::service::transactions::details::{TransactionDetails, TransactionData, MultisigExecutionDetails, DetailedExecutionInfo, MultisigConfirmation, ModuleExecutionDetails};
 use crate::models::service::transactions::{TransactionStatus, TransactionInfo, Custom};
 use crate::models::commons::{Operation, DataDecoded, Parameter};
 use crate::models::commons::ParamValue::SingleValue;
 
 #[test]
-fn multisig_transaction_to_transaction_details() {
+fn multisig_custom_transaction_to_transaction_details() {
     let multisig_tx = serde_json::from_str::<MultisigTransaction>(crate::json::MULTISIG_TX_CUSTOM).unwrap();
     let safe_info = serde_json::from_str::<SafeInfo>(crate::json::SAFE_WITH_MODULES).unwrap();
 
@@ -32,20 +32,20 @@ fn multisig_transaction_to_transaction_details() {
         }),
         tx_data: Some(TransactionData {
             hex_data: Some(String::from("0x095ea7b3000000000000000000000000ae9844f89d98c150f5e61bfc676d68b4921559900000000000000000000000000000000000000000000000000001c6bf52634000")),
-            data_decoded: Some(DataDecoded{
+            data_decoded: Some(DataDecoded {
                 method: "approve".to_string(),
                 parameters: Some(vec![
-                    Parameter{
+                    Parameter {
                         name: "spender".to_string(),
                         param_type: "address".to_string(),
-                        value: SingleValue(String::from("0xae9844F89D98c150F5e61bfC676D68b492155990"))
+                        value: SingleValue(String::from("0xae9844F89D98c150F5e61bfC676D68b492155990")),
                     },
-                    Parameter{
+                    Parameter {
                         name: "value".to_string(),
                         param_type: "uint256".to_string(),
-                        value: SingleValue(String::from("500000000000000"))
+                        value: SingleValue(String::from("500000000000000")),
                     }
-                ])
+                ]),
             }),
             to: "0xD9BA894E0097f8cC2BBc9D24D308b98e36dc6D02".to_string(),
             value: Some(String::from("0")),
@@ -64,17 +64,51 @@ fn multisig_transaction_to_transaction_details() {
                     String::from("0x65F8236309e5A99Ff0d129d04E486EBCE20DC7B0")],
                 confirmations_required: 2,
                 confirmations: vec![
-                    MultisigConfirmation{
+                    MultisigConfirmation {
                         signer: String::from("0x65F8236309e5A99Ff0d129d04E486EBCE20DC7B0"),
-                        signature: Some(String::from("0x83b1506c409918f21031e93ed2f62310a5e0c05b1be89242a6a266a7de4af7bc6094e206b33387b8d4465af6087a4d2158815e613aeb186d88d9a1973e00bbe81b")) },
-                    MultisigConfirmation{
+                        signature: Some(String::from("0x83b1506c409918f21031e93ed2f62310a5e0c05b1be89242a6a266a7de4af7bc6094e206b33387b8d4465af6087a4d2158815e613aeb186d88d9a1973e00bbe81b")),
+                    },
+                    MultisigConfirmation {
                         signer: String::from("0xF2CeA96575d6b10f51d9aF3b10e3e4E5738aa6bd"),
-                        signature: Some(String::from("0x000000000000000000000000f2cea96575d6b10f51d9af3b10e3e4e5738aa6bd000000000000000000000000000000000000000000000000000000000000000001")) },
+                        signature: Some(String::from("0x000000000000000000000000f2cea96575d6b10f51d9af3b10e3e4e5738aa6bd000000000000000000000000000000000000000000000000000000000000000001")),
+                    },
                 ],
             })),
     };
 
     let actual = MultisigTransaction::to_transaction_details(&multisig_tx, &mut mock_info_provider);
+
+    assert_eq!(expected, actual.unwrap());
+}
+
+#[test]
+fn module_transaction_to_transaction_details() {
+    let module_transaction = serde_json::from_str::<ModuleTransaction>(crate::json::MODULE_TX).unwrap();
+
+    let expected = TransactionDetails {
+        executed_at: Some(module_transaction.execution_date.timestamp_millis()),
+        tx_status: TransactionStatus::Success,
+        tx_hash: Some("0x705167e310ef0acb80a5f73eb4f8e66cfb32a896ac9380f3eb43e68ef8603a9f".to_string()),
+        tx_info: TransactionInfo::Custom(Custom {
+            to: "0xaAEb2035FF394fdB2C879190f95e7676f1A9444B".to_string(),
+            data_size: "132".to_string(),
+            value: "0".to_string(),
+        }),
+        tx_data: Some(TransactionData {
+            hex_data: Some(String::from("0x59f96ae500000000000000000000000000df91984582e6e96288307e9c2f20b38c8fece9000000000000000000000000c778417e063141139fce010982780140aa0cd5ab0000000000000000000000000000000000000000000000000000000000000475000000000000000000000000000000000000000000000003d962c8be3053def2")),
+            data_decoded: None,
+            to: "0xaAEb2035FF394fdB2C879190f95e7676f1A9444B".to_string(),
+            value: Some(String::from("0")),
+            operation: Operation::CALL,
+        }),
+        detailed_execution_info: Some(DetailedExecutionInfo::Module(
+            ModuleExecutionDetails {
+                address: "0xfa559f0932b7B60d90B4af0b8813d4088465096b".to_string()
+            })),
+    };
+
+
+    let actual = ModuleTransaction::to_transaction_details(&module_transaction);
 
     assert_eq!(expected, actual.unwrap());
 }

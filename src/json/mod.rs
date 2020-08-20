e11436a76064994db866105f71da70d7488b9468
Jose Pablo Alvarez Lacasia <jose.alvarez@gnosis.io>
2020-08-20 14:54:11 +0200

    Added test json for ERC20 and ERC721 transfer and their associated tokens

--- src/json/mod.rs
+++ src/json/mod.rs
@@ -9,8 +9,10 @@ pub const SAFE_WITH_MODULES: &str = include_str!("safes/with_modules.json");
 pub const _SAFE_WITH_MODULE_TXS: &str = include_str!("safes/with_module_transactions.json");
 
 pub const ETHER_TRANSFER: &str = include_str!("transfers/ether_transfer.json");
+pub const ERC_20_TRANSFER: &str = include_str!("transfers/erc_20_transfer.json");
+pub const ERC_721_TRANSFER: &str = include_str!("transfers/erc_721_transfer.json");
 
-pub const TOKEN_USDT: &str = include_str!("tokens/USDT.json");
+pub const TOKEN_USDT: &str = include_str!("tokens/usdt.json");
 pub const TOKEN_CRYPTO_KITTIES: &str = include_str!("tokens/crypto_kitties.json");
 
 pub const DATA_DECODED_MULTI_SEND: &str = include_str!("commons/data_decoded_multi_send.json");

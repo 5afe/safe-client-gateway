a385121e3f6fb34a6de22f6f56c350ac8381356d
Richard Meissner <richard@gnosis.io>
2020-08-24 11:41:15 +0200

    Invalidate cache with safe tx hash if possible

--- src/services/hooks.rs
+++ src/services/hooks.rs
@@ -1,7 +1,23 @@
 use crate::utils::context::Context;
+use crate::models::backend::webhooks::{Payload, PayloadDetails};
 use anyhow::Result;
 
-pub fn invalidate_caches(context: &Context, safe: &String) -> Result<()> {
-    context.cache().invalidate_pattern(&format!("*{}*", safe));
+pub fn invalidate_caches(context: &Context, payload: &Payload) -> Result<()> {
+    let cache = context.cache();
+    cache.invalidate_pattern(&format!("*{}*", &payload.address));
+    payload.details.map(|d| {
+        match d {
+            PayloadDetails::NewConfirmation(data) => {
+                cache.invalidate_pattern(&format!("*{}*", data.safe_tx_hash));
+            },
+            PayloadDetails::ExecutedMultisigTransaction(data) => {
+                cache.invalidate_pattern(&format!("*{}*", data.safe_tx_hash));
+            },
+            PayloadDetails::PendingMultisigTransaction(data) => {
+                cache.invalidate_pattern(&format!("*{}*", data.safe_tx_hash));
+            },
+            _ => {}
+        }
+    });
     Ok(())
 }

af5505f12bbdf79d2206d278b52fa6307993bbec
Jose Pablo Alvarez Lacasia <jose.alvarez@gnosis.io>
2020-08-26 15:24:59 +0200

    Added new confirmation payload webhook test

--- src/services/hooks.rs
+++ src/services/hooks.rs
@@ -6,10 +6,12 @@ use crate::utils::cache::Cache;
 pub fn invalidate_caches(context: &impl ContextCache, payload: &Payload) -> Result<()> {
     let cache = context.cache();
     cache.invalidate_pattern(&format!("*{}*", &payload.address));
+    println!("{:#?}", &payload);
 
     payload.details.as_ref().map(|d| {
         match d {
             PayloadDetails::NewConfirmation(data) => {
+                println!("{:#?}", &data);
                 cache.invalidate_pattern(&format!("*{}*", data.safe_tx_hash));
             }
             PayloadDetails::ExecutedMultisigTransaction(data) => {
--- src/services/tests/invalidate_caches.rs
+++ src/services/tests/invalidate_caches.rs
@@ -1,8 +1,9 @@
-use crate::models::backend::webhooks::Payload;
+use crate::models::backend::webhooks::{Payload, PayloadDetails, NewConfirmation};
 use crate::services::hooks::invalidate_caches;
 use mockall::predicate::*;
 use crate::utils::cache::*;
 use crate::utils::context::ContextCache;
+use mockall::Sequence;
 
 struct TestContext(MockCache);
 
@@ -29,15 +30,62 @@ fn invalidate_with_empty_payload() {
         .expect_create()
         .times(0);
     mock_cache
+        .expect_invalidate()
+        .times(0);
+    mock_cache
         .expect_invalidate_pattern()
         .with(eq(String::from("*0x1230B3d59858296A31053C1b8562Ecf89A2f888b*")))
         .return_const(())
         .times(1);
-    mock_cache
-        .expect_invalidate()
-        .times(0);
 
     let context = TestContext(mock_cache);
 
     invalidate_caches(&context, &payload).unwrap();
 }
+
+#[test]
+fn invalidate_new_confirmation_payload() {
+    let payload = Payload {
+        address: "0x1230B3d59858296A31053C1b8562Ecf89A2f888b".to_string(),
+        details: Some(PayloadDetails::NewConfirmation(
+            NewConfirmation {
+                owner: "0x65F8236309e5A99Ff0d129d04E486EBCE20DC7B0".to_string(),
+                safe_tx_hash: "0x65df8a1e5a40703d9c67d5df6f9b552d3830faf0507c3d7350ba3764d3a68621".to_string(),
+            })),
+    };
+
+    let mut mock_cache = MockCache::new();
+    let mut sequence = Sequence::new();
+    mock_cache
+        .expect_fetch()
+        .times(0);
+    mock_cache
+        .expect_create()
+        .times(0);
+    mock_cache
+        .expect_invalidate()
+        .times(0);
+    mock_cache
+        .expect_invalidate_pattern()
+        .times(1)
+        .return_const(())
+        .with(eq(String::from("*0x1230B3d59858296A31053C1b8562Ecf89A2f888b*")))
+        .in_sequence(&mut sequence);
+
+    mock_cache
+        .expect_invalidate_pattern()
+        .times(1)
+        .return_const(())
+        .with(eq(String::from("*0x65df8a1e5a40703d9c67d5df6f9b552d3830faf0507c3d7350ba3764d3a68621*")))
+        .in_sequence(&mut sequence);
+
+    let context = TestContext(mock_cache);
+
+    invalidate_caches(&context, &payload).unwrap();
+}
+
+#[test]
+fn invalidate_executed_multisig_transaction_payload() {}
+
+#[test]
+fn invalidate_pending_multisig_transaction_payload() {}

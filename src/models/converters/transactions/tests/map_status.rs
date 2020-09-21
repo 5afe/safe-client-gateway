d44e0f6ad119e1a51e6befbf09c297e26cffaea7
Jose Pablo Alvarez Lacasia <jose.alvarez@gnosis.io>
2020-09-21 21:57:20 +0200

    Added test for failed status

--- src/models/converters/transactions/tests/map_status.rs
+++ src/models/converters/transactions/tests/map_status.rs
@@ -3,12 +3,19 @@ use crate::providers::info::SafeInfo;
 use crate::models::service::transactions::TransactionStatus;
 
 #[test]
-pub (super) fn map_status_to_awaiting_confirmation() {
-    let tx_json = crate::json::MULTISIG_TX_ETHER_TRANSFER;
-    let safe_info_json = crate::json::SAFE_WITH_MODULES;
-    let tx = serde_json::from_str::<MultisigTransaction>(tx_json).unwrap();
-    let safe_info = serde_json::from_str::<SafeInfo>(safe_info_json).unwrap();
+fn map_status_to_success() {
+    let tx = serde_json::from_str::<MultisigTransaction>(crate::json::MULTISIG_TX_ETHER_TRANSFER).unwrap();
+    let safe_info = serde_json::from_str::<SafeInfo>(crate::json::SAFE_WITH_MODULES).unwrap();
     let actual = tx.map_status(&safe_info);
 
     assert_eq!(TransactionStatus::Success, actual);
+}
+
+#[test]
+fn map_status_to_failed() {
+    let tx = serde_json::from_str::<MultisigTransaction>(crate::json::MULTISIG_FAILED_TX).unwrap();
+    let safe_info = serde_json::from_str::<SafeInfo>(crate::json::SAFE_WITH_MODULES).unwrap();
+    let actual = tx.map_status(&safe_info);
+
+    assert_eq!(TransactionStatus::Failed, actual);
 }

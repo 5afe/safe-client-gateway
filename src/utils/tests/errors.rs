a51558dbb76ab1704cf09b3ab61697a982e1da22
Jose Pablo Alvarez Lacasia <jose.alvarez@gnosis.io>
2020-09-24 15:17:13 +0200

    Added wrapping mod test for branch exclussion for grcov

--- src/utils/tests/errors.rs
+++ src/utils/tests/errors.rs
@@ -1,101 +1,103 @@
-use rocket::local::Client;
-use crate::utils::errors::{ApiError, ErrorDetails};
-use rocket::response::Responder;
-use crate::models::backend::transactions::MultisigTransaction;
+mod test {
+    use rocket::local::Client;
+    use crate::utils::errors::{ApiError, ErrorDetails};
+    use rocket::response::Responder;
+    use crate::models::backend::transactions::MultisigTransaction;
 
-#[test]
-fn api_error_responder_json() {
-    let api_error = ApiError {
-        status: 418,
-        details: ErrorDetails {
+    #[test]
+    fn api_error_responder_json() {
+        let api_error = ApiError {
+            status: 418,
+            details: ErrorDetails {
+                code: 1337,
+                message: Some("Not found".to_string()),
+                arguments: None,
+            },
+        };
+        let expected_error_json = r#"{"code":1337,"message":"Not found"}"#;
+
+        let rocket = rocket::ignite();
+        let client = Client::new(rocket).expect("valid rocket instance");
+        let local_request = client.get("/");
+        let request = local_request.inner();
+        let mut response = api_error.respond_to(&request).unwrap();
+
+        let body_json = response.body().unwrap().into_string().unwrap();
+
+        assert_eq!(response.status().code, 418);
+        assert_eq!(body_json, expected_error_json);
+    }
+
+    #[test]
+    fn api_error_from_anyhow_error() {
+        let error = anyhow::anyhow!("Error message");
+        let error_details = ErrorDetails {
             code: 1337,
-            message: Some("Not found".to_string()),
+            message: Some("Error message".to_string()),
             arguments: None,
-        },
-    };
-    let expected_error_json = r#"{"code":1337,"message":"Not found"}"#;
+        };
 
-    let rocket = rocket::ignite();
-    let client = Client::new(rocket).expect("valid rocket instance");
-    let local_request = client.get("/");
-    let request = local_request.inner();
-    let mut response = api_error.respond_to(&request).unwrap();
+        let actual = ApiError::from(error);
 
-    let body_json = response.body().unwrap().into_string().unwrap();
+        assert_eq!(actual.status, 500);
+        assert_eq!(actual.details, error_details);
+    }
 
-    assert_eq!(response.status().code, 418);
-    assert_eq!(body_json, expected_error_json);
-}
+    #[test]
+    fn api_error_from_serde_error() {
+        let error = serde_json::from_str::<MultisigTransaction>("{").expect_err("Error message");
+        let error_details = ErrorDetails {
+            code: 1337,
+            message: Some(format!("{:?}", &error)),
+            arguments: None,
+        };
 
-#[test]
-fn api_error_from_anyhow_error() {
-    let error = anyhow::anyhow!("Error message");
-    let error_details = ErrorDetails {
-        code: 1337,
-        message: Some("Error message".to_string()),
-        arguments: None,
-    };
+        let actual = ApiError::from(error);
 
-    let actual = ApiError::from(error);
+        assert_eq!(actual.status, 500);
+        assert_eq!(actual.details, error_details);
+    }
 
-    assert_eq!(actual.status, 500);
-    assert_eq!(actual.details, error_details);
-}
-
-#[test]
-fn api_error_from_serde_error() {
-    let error = serde_json::from_str::<MultisigTransaction>("{").expect_err("Error message");
-    let error_details = ErrorDetails {
-        code: 1337,
-        message: Some(format!("{:?}", &error)),
-        arguments: None,
-    };
-
-    let actual = ApiError::from(error);
-
-    assert_eq!(actual.status, 500);
-    assert_eq!(actual.details, error_details);
-}
-
-#[test]
-fn api_error_known_error_json_structure() {
-    let expected_error_json = r#"{
+    #[test]
+    fn api_error_known_error_json_structure() {
+        let expected_error_json = r#"{
         "code": 1,
         "message": "Checksum address validation failed",
         "arguments": [
           "0x1230b3d59858296A31053C1b8562Ecf89A2f888b"
         ]
     }"#;
-    let expected_error = ErrorDetails {
-        code: 1,
-        message: Some("Checksum address validation failed".to_string()),
-        arguments: Some(vec!["0x1230b3d59858296A31053C1b8562Ecf89A2f888b".to_string()]),
-    };
+        let expected_error = ErrorDetails {
+            code: 1,
+            message: Some("Checksum address validation failed".to_string()),
+            arguments: Some(vec!["0x1230b3d59858296A31053C1b8562Ecf89A2f888b".to_string()]),
+        };
 
-    let actual = ApiError::from_backend_error(422, &expected_error_json);
+        let actual = ApiError::from_backend_error(422, &expected_error_json);
 
-    assert_eq!(actual.status, 422);
-    assert_eq!(actual.details, expected_error);
-}
+        assert_eq!(actual.status, 422);
+        assert_eq!(actual.details, expected_error);
+    }
 
-#[test]
-fn api_error_unknown_error_json_structure() {
-    let expected_error_json = r#"{
+    #[test]
+    fn api_error_unknown_error_json_structure() {
+        let expected_error_json = r#"{
         "code": 1,
         "message": ["Checksum address validation failed"],
         "arguments": [
           "0x1230b3d59858296A31053C1b8562Ecf89A2f888b"
         ]
     }"#;
-    let expected_error = ErrorDetails {
-        code: 42,
-        message: Some(expected_error_json.to_owned()),
-        arguments: None,
-    };
+        let expected_error = ErrorDetails {
+            code: 42,
+            message: Some(expected_error_json.to_owned()),
+            arguments: None,
+        };
 
 
-    let actual = ApiError::from_backend_error(422, &expected_error_json);
+        let actual = ApiError::from_backend_error(422, &expected_error_json);
 
-    assert_eq!(actual.status, 422);
-    assert_eq!(actual.details, expected_error);
+        assert_eq!(actual.status, 422);
+        assert_eq!(actual.details, expected_error);
+    }
 }
--- src/utils/tests/json.rs
+++ src/utils/tests/json.rs
@@ -1,88 +1,90 @@
-use crate::utils::json::default_if_null;
-use serde::Deserialize;
+mod test {
+    use crate::utils::json::default_if_null;
+    use serde::Deserialize;
 
 
-#[derive(PartialEq, Deserialize, Debug)]
-struct ExpectedStruct {
-    #[serde(deserialize_with = "default_if_null")]
-    field: u64,
-    other_field: String,
-    #[serde(deserialize_with = "default_if_null")]
-    with_custom_default: WithCustomDefault,
-}
-
-#[derive(PartialEq, Deserialize, Debug)]
-struct WithCustomDefault(String);
-
-impl Default for WithCustomDefault {
-    fn default() -> Self {
-        WithCustomDefault("custom default".to_string())
+    #[derive(PartialEq, Deserialize, Debug)]
+    struct ExpectedStruct {
+        #[serde(deserialize_with = "default_if_null")]
+        field: u64,
+        other_field: String,
+        #[serde(deserialize_with = "default_if_null")]
+        with_custom_default: WithCustomDefault,
     }
-}
 
-#[test]
-#[should_panic]
-fn deserialize_missing_expected_field() {
-    let test_json = json!({
+    #[derive(PartialEq, Deserialize, Debug)]
+    struct WithCustomDefault(String);
+
+    impl Default for WithCustomDefault {
+        fn default() -> Self {
+            WithCustomDefault("custom default".to_string())
+        }
+    }
+
+    #[test]
+    #[should_panic]
+    fn deserialize_missing_expected_field() {
+        let test_json = json!({
         "other_field":"other value"
     });
 
-    serde_json::from_str::<ExpectedStruct>(&test_json.to_string()).unwrap();
-}
+        serde_json::from_str::<ExpectedStruct>(&test_json.to_string()).unwrap();
+    }
 
-#[test]
-fn deserialize_expected_field_null() {
-    let test_json = json!({
+    #[test]
+    fn deserialize_expected_field_null() {
+        let test_json = json!({
         "field" : null,
         "other_field":"other value",
         "with_custom_default": "different value"
     });
 
-    let expected = ExpectedStruct {
-        field: 0,
-        other_field: "other value".to_string(),
-        with_custom_default: WithCustomDefault("different value".to_string()),
-    };
+        let expected = ExpectedStruct {
+            field: 0,
+            other_field: "other value".to_string(),
+            with_custom_default: WithCustomDefault("different value".to_string()),
+        };
 
-    let actual = serde_json::from_str::<ExpectedStruct>(&test_json.to_string()).unwrap();
+        let actual = serde_json::from_str::<ExpectedStruct>(&test_json.to_string()).unwrap();
 
-    assert_eq!(expected, actual);
-}
+        assert_eq!(expected, actual);
+    }
 
-#[test]
-fn deserialize_expected_field_with_value() {
-    let test_json = json!({
+    #[test]
+    fn deserialize_expected_field_with_value() {
+        let test_json = json!({
         "field" : 42,
         "other_field":"other value",
         "with_custom_default": "different value"
     });
 
-    let expected = ExpectedStruct {
-        field: 42,
-        other_field: "other value".to_string(),
-        with_custom_default: WithCustomDefault("different value".to_string()),
-    };
+        let expected = ExpectedStruct {
+            field: 42,
+            other_field: "other value".to_string(),
+            with_custom_default: WithCustomDefault("different value".to_string()),
+        };
 
-    let actual = serde_json::from_str::<ExpectedStruct>(&test_json.to_string()).unwrap();
+        let actual = serde_json::from_str::<ExpectedStruct>(&test_json.to_string()).unwrap();
 
-    assert_eq!(expected, actual);
-}
+        assert_eq!(expected, actual);
+    }
 
-#[test]
-fn deserialize_expected_field_with_custom_default() {
-    let test_json = json!({
+    #[test]
+    fn deserialize_expected_field_with_custom_default() {
+        let test_json = json!({
         "field" : 42,
         "other_field":"other value",
         "with_custom_default": null
     });
 
-    let expected = ExpectedStruct {
-        field: 42,
-        other_field: "other value".to_string(),
-        with_custom_default: WithCustomDefault("custom default".to_string()),
-    };
+        let expected = ExpectedStruct {
+            field: 42,
+            other_field: "other value".to_string(),
+            with_custom_default: WithCustomDefault("custom default".to_string()),
+        };
 
-    let actual = serde_json::from_str::<ExpectedStruct>(&test_json.to_string()).unwrap();
+        let actual = serde_json::from_str::<ExpectedStruct>(&test_json.to_string()).unwrap();
 
-    assert_eq!(expected, actual);
+        assert_eq!(expected, actual);
+    }
 }

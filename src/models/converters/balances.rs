c2cdc3776178a82dc091e36701a35ad1cef467a3
Jose Pablo Alvarez Lacasia <jose.alvarez@gnosis.io>
2020-10-07 15:33:43 +0200

    Renamed internal variable

--- src/models/converters/balances.rs
+++ src/models/converters/balances.rs
@@ -5,7 +5,7 @@ use crate::providers::info::{TokenInfo, TokenType};
 impl BalanceDto {
     pub fn to_balance(&self, usd_to_fiat: f64) -> Balance {
         let fiat_conversion = self.fiat_conversion.parse::<f64>().unwrap_or(0.0) * usd_to_fiat;
-        let fiat_amount = self.fiat_balance.parse::<f64>().unwrap_or(0.0) * usd_to_fiat;
+        let fiat_balance = self.fiat_balance.parse::<f64>().unwrap_or(0.0) * usd_to_fiat;
         let token_type = self.token_address.as_ref().map(|_| TokenType::Erc20).unwrap_or(TokenType::Ether);
         Balance {
             token_info: TokenInfo {
@@ -17,7 +17,7 @@ impl BalanceDto {
                 logo_uri: self.token.as_ref().map(|it| it.logo_uri.to_string()),
             },
             balance: self.balance.to_owned(),
-            fiat_balance: fiat_amount.to_string(),
+            fiat_balance: fiat_balance.to_string(),
             fiat_conversion: fiat_conversion.to_string(),
         }
     }

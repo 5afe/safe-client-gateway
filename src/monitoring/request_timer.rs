554c06d8f914e2887b5cc949ac9978f579a7a57c
Jose Pablo Alvarez Lacasia <jose.alvarez@gnosis.io>
2020-10-08 14:35:14 +0200

    Removed unnecessary logs

--- src/monitoring/request_timer.rs
+++ src/monitoring/request_timer.rs
@@ -13,15 +13,11 @@ impl Fairing for RequestTimer {
     }
 
     fn on_request(&self, request: &mut Request, _data: &Data) {
-        let instant = Utc::now();
-        log::info!("on_request: {}", &instant.to_rfc3339());
-        request.local_cache(|| instant.timestamp_millis());
+        request.local_cache(|| Utc::now().timestamp_millis());
     }
 
     fn on_response(&self, request: &Request, _response: &mut Response) {
-        let instant = Utc::now();
-        log::info!("on_response: {}", &instant.to_rfc3339());
-        let cached = request.local_cache(|| instant.timestamp_millis()).to_owned();
+        let cached = request.local_cache(|| Utc::now().timestamp_millis()).to_owned();
         let delta = Utc::now().timestamp_millis() - cached;
         log::info!("For endpoint {} the request processing duration was {} [ms]", request.uri().path(), delta)
     }

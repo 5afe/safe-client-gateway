a14cdb5e075a2c555a38b8f4336c3bc2709e92b7
Jose Pablo Alvarez Lacasia <jose.alvarez@gnosis.io>
2020-10-08 11:13:21 +0200

    Added Fairing for measuring duration of request processing

---
+++ src/monitoring/request_timer.rs
@@ -0,0 +1,30 @@
+use rocket::fairing::{Fairing, Info, Kind};
+use chrono::Utc;
+use rocket::{Request, Response, Data};
+use rocket::http::Header;
+
+pub struct RequestTimer();
+
+impl Fairing for RequestTimer {
+    fn info(&self) -> Info {
+        Info {
+            name: "RequestTimer",
+            kind: Kind::Request | Kind::Response,
+        }
+    }
+
+    fn on_request(&self, request: &mut Request, _data: &Data) {
+        let instant = Utc::now().timestamp_millis();
+        log::debug!("on_request: {}", &instant);
+        let h = Header::new("requested_timestamp", instant.to_string());
+        request.add_header(h)
+    }
+
+    fn on_response(&self, request: &Request, _response: &mut Response) {
+        let h = request.headers().get("requested_timestamp").next().unwrap().parse::<i64>().unwrap();
+        let instant = Utc::now().timestamp_millis();
+
+        let delta = instant - h;
+        log::debug!("For endpoint {} the request processing duration was {} [ms]", request.uri().path(), delta)
+    }
+}

51f03ff797beedb8e835d67221d11db13275cc10
Jose Alvarez <jpalvarezl@users.noreply.github.com>
2020-10-21 15:03:28 +0200

    Forwared trust and exclud_spam boolean flag filters (#170)

--- src/routes/balances.rs
+++ src/routes/balances.rs
@@ -5,15 +5,25 @@ use crate::utils::context::Context;
 use crate::utils::errors::ApiResult;
 use rocket::response::content;
 
-#[get("/v1/safes/<safe_address>/balances/<fiat>")]
+#[get("/v1/safes/<safe_address>/balances/<fiat>?<trusted>&<exclude_spam>")]
 pub fn get_balances(
     context: Context,
     safe_address: String,
     fiat: String,
+    trusted: Option<bool>,
+    exclude_spam: Option<bool>,
 ) -> ApiResult<content::Json<String>> {
+    let trusted = trusted.unwrap_or(false);
+    let exclude_spam = exclude_spam.unwrap_or(false);
     context
         .cache()
         .cache_resp(&context.uri(), request_cache_duration(), || {
-            balances(&context, safe_address.as_str(), fiat.as_str())
+            balances(
+                &context,
+                safe_address.as_str(),
+                fiat.as_str(),
+                trusted,
+                exclude_spam,
+            )
         })
 }

8408e6b43a37bbb80efc060506fed1bc575d85ab
Jose Pablo Alvarez Lacasia <jose.alvarez@gnosis.io>
2020-12-02 17:59:58 +0100

    Completed unit tests for poor parser

--- src/models/converters/tests/page_metadata.rs
+++ src/models/converters/tests/page_metadata.rs
@@ -13,7 +13,25 @@ fn page_metadata_with_valid_non_zero_data() {
 }
 
 #[test]
-fn page_metadata_with_zeros() {}
+fn page_metadata_with_zeros() {
+    let input = "limit=0&offset=0";
+
+    let actual = PageMetadata::from_url_string(input).unwrap();
+    let expected = PageMetadata {
+        offset: 0,
+        limit: 0,
+    };
+    assert_eq!(expected, actual);
+}
 
 #[test]
-fn page_metadata_with_missing_optional_args() {}
+fn page_metadata_with_missing_optional_args() {
+    let input = "offset=50";
+
+    let actual = PageMetadata::from_url_string(input).unwrap();
+    let expected = PageMetadata {
+        offset: 50,
+        limit: 0,
+    };
+    assert_eq!(expected, actual);
+}

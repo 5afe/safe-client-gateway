34c23c43319185849c722f5b82945cf8c278e4bf
Jose Pablo Alvarez Lacasia <jose.alvarez@gnosis.io>
2020-12-03 10:13:17 +0100

    Moving forward in pages of data with page peeking works, backwards is broken

--- src/services/transactions_history.rs
+++ src/services/transactions_history.rs
@@ -2,7 +2,7 @@ extern crate reqwest;
 
 use crate::config::{base_transaction_service_url, request_cache_duration};
 use crate::models::backend::transactions::Transaction;
-use crate::models::commons::Page;
+use crate::models::commons::{Page, PageMetadata};
 use crate::models::service::transactions::summary::{
     ConflictType, TransactionListItem, TransactionSummary,
 };
@@ -14,7 +14,7 @@ use crate::utils::errors::ApiResult;
 use crate::utils::extract_query_string;
 use chrono::{DateTime, Datelike, NaiveDate, NaiveDateTime, Utc};
 use itertools::Itertools;
-use log::debug;
+use std::cmp::max;
 
 pub fn get_history_transactions(
     context: &Context,
@@ -22,19 +22,24 @@ pub fn get_history_transactions(
     page_url: &Option<String>,
     timezone_offset: &Option<String>,
 ) -> ApiResult<Page<TransactionListItem>> {
+    let is_first_page = page_url.is_none();
+    let page_metadata = build_page_metadata_extended_size(page_url);
+
     let mut info_provider = DefaultInfoProvider::new(context);
     let url = format!(
         "{}/v1/safes/{}/all-transactions/?{}&queued=false",
         base_transaction_service_url(),
         safe_address,
-        page_url.as_ref().unwrap_or(&String::new())
+        page_metadata.to_url_string()
     );
     let body = context
         .cache()
         .request_cached(&context.client(), &url, request_cache_duration())?;
-    debug!("request URL: {}", &url);
-    debug!("page_url: {:#?}", page_url);
+    log::error!("request URL: {}", &url);
+    log::error!("page_url: {:#?}", page_url);
+    log::error!("page_metadata: {:#?}", page_metadata);
     let backend_transactions: Page<Transaction> = serde_json::from_str(&body)?;
+    log::error!("RETURNED COUNT: {}", &backend_transactions.results.len()); //should always be 21
     let mut service_transactions: Vec<TransactionSummary> = backend_transactions
         .results
         .into_iter()
@@ -72,9 +77,10 @@ pub fn get_history_transactions(
             .as_ref()
             .and_then(|link| extract_query_string(link))
             .map(|link| {
+                log::error!("NEXT LINK: {}", &link);
                 context.build_absolute_url(uri!(
                     crate::routes::transactions::history_transactions: safe_address,
-                    link,
+                    restore_page_size(link, is_first_page),
                     "" // timezone_offset
                 ))
             }),
@@ -85,12 +91,41 @@ pub fn get_history_transactions(
             .map(|link| {
                 context.build_absolute_url(uri!(
                     crate::routes::transactions::history_transactions: safe_address,
-                    link,
+                    restore_page_size(link, is_first_page),
                     "" // timezone_offset
                 ))
             }),
         results: service_transactions_with_dates,
     })
+}
+
+fn build_page_metadata_extended_size(page_url: &Option<String>) -> PageMetadata {
+    if let Some(page_url_str) = page_url {
+        let page_metadata = PageMetadata::from_url_string(page_url_str).unwrap();
+        PageMetadata {
+            limit: page_metadata.limit + 1,
+            offset: page_metadata.offset - 1,
+        }
+    } else {
+        // first page
+        PageMetadata {
+            limit: 21,
+            offset: 0,
+        }
+    }
+}
+
+fn restore_page_size(link: String, is_first_page: bool) -> String {
+    let page_metadata = PageMetadata::from_url_string(&link).unwrap();
+    let page_metadata = PageMetadata {
+        offset: if is_first_page {
+            20
+        } else {
+            page_metadata.offset
+        },
+        limit: 20,
+    };
+    page_metadata.to_url_string()
 }
 
 fn get_day_timestamp_millis(timestamp_in_secs: i64) -> i64 {

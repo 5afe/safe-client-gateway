489a21c6532c33c216f5b3b4acb0372e8c10b32d
Jose Pablo Alvarez Lacasia <jose.alvarez@gnosis.io>
2020-12-09 14:01:11 +0100

    Added trusted and exclude_spam filter flags for forwarding

--- src/routes/collectibles.rs
+++ src/routes/collectibles.rs
@@ -5,12 +5,21 @@ use crate::utils::context::Context;
 use crate::utils::errors::ApiResult;
 use rocket::response::content;
 
-#[get("/v1/safes/<safe_address>/collectibles")]
-pub fn list(context: Context, safe_address: String) -> ApiResult<content::Json<String>> {
+#[get("/v1/safes/<safe_address>/collectibles?<trusted>&<exclude_spam>")]
+pub fn list(
+    context: Context,
+    safe_address: String,
+    trusted: Option<bool>,
+    exclude_spam: Option<bool>,
+) -> ApiResult<content::Json<String>> {
+    let trusted = trusted.unwrap_or(false);
+    let exclude_spam = exclude_spam.unwrap_or(true);
     let url = format!(
-        "{}/v1/safes/{}/collectibles",
+        "{}/v1/safes/{}/collectibles?trusted={}&exclude_spam={}",
         base_transaction_service_url(),
-        safe_address
+        safe_address,
+        trusted,
+        exclude_spam
     );
     Ok(content::Json(context.cache().request_cached(
         &context.client(),

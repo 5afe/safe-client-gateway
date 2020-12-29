c446defef201c85e9273155e70fe419720557862
Jose Pablo Alvarez Lacasia <jose.alvarez@gnosis.io>
2020-12-29 12:22:23 +0100

    Refactored requests for sending funds to be polymorphic

--- src/models/service/transactions/requests.rs
+++ src/models/service/transactions/requests.rs
@@ -6,12 +6,29 @@ pub struct ConfirmationRequest {
     pub signed_safe_tx_hash: String,
 }
 
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+#[serde(tag = "type")]
+pub enum SendFundsRequest {
+    Ether(SendEthRequest),
+    Erc20(SendErc20Request),
+}
+
+#[derive(Debug, Deserialize, Serialize, PartialEq)]
 #[serde(rename_all = "camelCase")]
 pub struct SendEthRequest {
     pub receiver: String,
     pub sender: String,
     pub value: String,
+    pub transaction_hash: String,
+    pub signed_transaction_hash: String,
+    pub nonce: String,
+}
+
+#[derive(Debug, Deserialize, Serialize, PartialEq)]
+#[serde(rename_all = "camelCase")]
+pub struct SendErc20Request {
+    pub receiver: String,
+    pub sender: String,
     pub data: String,
     pub transaction_hash: String,
     pub signed_transaction_hash: String,

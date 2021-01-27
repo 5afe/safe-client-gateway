783e457327517deb73a42247298d111768033063
Jose Alvarez <jp.alvarezl@gmail.com>
2021-01-27 07:48:04 +0100

    Added happy path tests for data_decoded module

--- src/models/converters/tests/data_decoded.rs
+++ src/models/converters/tests/data_decoded.rs
@@ -1,5 +1,6 @@
 use crate::models::commons::{DataDecoded, ParamValue, Parameter};
 use crate::models::service::transactions::{SettingsChange, SettingsInfo};
+use crate::providers::address_info::AddressInfo;
 use crate::providers::info::*;
 
 #[test]
@@ -19,6 +20,39 @@ fn data_decoded_set_fallback_handler_to_settings_info() {
         settings_info: Some(SettingsInfo::SetFallbackHandler {
             handler: "0xd5D82B6aDDc9027B22dCA772Aa68D5d74cdBdF44".to_string(),
             handler_info: None,
+        }),
+    };
+
+    let actual = DataDecoded::to_settings_info(&data_decoded, &mut mock_info_provider);
+
+    assert_eq!(expected.settings_info, actual);
+}
+
+#[test]
+fn data_decoded_set_fallback_handler_to_settings_info_with_address_info() {
+    let mut mock_info_provider = MockInfoProvider::new();
+    mock_info_provider
+        .expect_address_info()
+        .times(1)
+        .return_once(move |_| {
+            Ok(AddressInfo {
+                name: "Address name".to_string(),
+                logo_uri: Some("logo.url".to_string()),
+            })
+        });
+
+    let data_decoded =
+        serde_json::from_str::<DataDecoded>(crate::json::DATA_DECODED_SET_FALLBACK_HANDLER)
+            .unwrap();
+
+    let expected = SettingsChange {
+        data_decoded: data_decoded.clone(),
+        settings_info: Some(SettingsInfo::SetFallbackHandler {
+            handler: "0xd5D82B6aDDc9027B22dCA772Aa68D5d74cdBdF44".to_string(),
+            handler_info: Some(AddressInfo {
+                name: "Address name".to_string(),
+                logo_uri: Some("logo.url".to_string()),
+            }),
         }),
     };
 
@@ -54,6 +88,40 @@ fn data_decoded_add_owner_with_threshold_to_settings_info() {
 }
 
 #[test]
+fn data_decoded_add_owner_with_threshold_to_settings_info_with_address_info() {
+    let mut mock_info_provider = MockInfoProvider::new();
+    mock_info_provider
+        .expect_address_info()
+        .times(1)
+        .return_once(move |_| {
+            Ok(AddressInfo {
+                name: "Address name".to_string(),
+                logo_uri: Some("logo.url".to_string()),
+            })
+        });
+
+    let data_decoded =
+        serde_json::from_str::<DataDecoded>(crate::json::DATA_DECODED_ADD_OWNER_WITH_THRESHOLD)
+            .unwrap();
+
+    let expected = SettingsChange {
+        data_decoded: data_decoded.clone(),
+        settings_info: Some(SettingsInfo::AddOwner {
+            owner: "0xBEA2F9227230976d2813a2f8b922c22bE1DE1B23".to_string(),
+            owner_info: Some(AddressInfo {
+                name: "Address name".to_string(),
+                logo_uri: Some("logo.url".to_string()),
+            }),
+            threshold: 1,
+        }),
+    };
+
+    let actual = DataDecoded::to_settings_info(&data_decoded, &mut mock_info_provider);
+
+    assert_eq!(expected.settings_info, actual);
+}
+
+#[test]
 fn data_decoded_remove_owner_to_settings_info() {
     let mut mock_info_provider = MockInfoProvider::new();
     mock_info_provider
@@ -69,6 +137,39 @@ fn data_decoded_remove_owner_to_settings_info() {
         settings_info: Some(SettingsInfo::RemoveOwner {
             owner: "0xF2CeA96575d6b10f51d9aF3b10e3e4E5738aa6bd".to_string(),
             owner_info: None,
+            threshold: 2,
+        }),
+    };
+
+    let actual = DataDecoded::to_settings_info(&data_decoded, &mut mock_info_provider);
+
+    assert_eq!(expected.settings_info, actual);
+}
+
+#[test]
+fn data_decoded_remove_owner_to_settings_info_with_address_info() {
+    let mut mock_info_provider = MockInfoProvider::new();
+    mock_info_provider
+        .expect_address_info()
+        .times(1)
+        .return_once(move |_| {
+            Ok(AddressInfo {
+                name: "Address name".to_string(),
+                logo_uri: Some("logo.url".to_string()),
+            })
+        });
+
+    let data_decoded =
+        serde_json::from_str::<DataDecoded>(crate::json::DATA_DECODED_REMOVE_OWNER).unwrap();
+
+    let expected = SettingsChange {
+        data_decoded: data_decoded.clone(),
+        settings_info: Some(SettingsInfo::RemoveOwner {
+            owner: "0xF2CeA96575d6b10f51d9aF3b10e3e4E5738aa6bd".to_string(),
+            owner_info: Some(AddressInfo {
+                name: "Address name".to_string(),
+                logo_uri: Some("logo.url".to_string()),
+            }),
             threshold: 2,
         }),
     };
@@ -96,6 +197,43 @@ fn data_decoded_swap_owner_to_settings_info() {
             old_owner_info: None,
             new_owner: "0xF2CeA96575d6b10f51d9aF3b10e3e4E5738aa6bd".to_string(),
             new_owner_info: None,
+        }),
+    };
+
+    let actual = DataDecoded::to_settings_info(&data_decoded, &mut mock_info_provider);
+
+    assert_eq!(expected.settings_info, actual);
+}
+
+#[test]
+fn data_decoded_swap_owner_to_settings_info_with_address_info() {
+    let mut mock_info_provider = MockInfoProvider::new();
+    mock_info_provider
+        .expect_address_info()
+        .times(2)
+        .returning(move |_| {
+            Ok(AddressInfo {
+                name: "Address name".to_string(),
+                logo_uri: Some("logo.url".to_string()),
+            })
+        });
+
+    let data_decoded =
+        serde_json::from_str::<DataDecoded>(crate::json::DATA_DECODED_SWAP_OWNER).unwrap();
+
+    let expected = SettingsChange {
+        data_decoded: data_decoded.clone(),
+        settings_info: Some(SettingsInfo::SwapOwner {
+            old_owner: "0xA3DAa0d9Ae02dAA17a664c232aDa1B739eF5ae8D".to_string(),
+            old_owner_info: Some(AddressInfo {
+                name: "Address name".to_string(),
+                logo_uri: Some("logo.url".to_string()),
+            }),
+            new_owner: "0xF2CeA96575d6b10f51d9aF3b10e3e4E5738aa6bd".to_string(),
+            new_owner_info: Some(AddressInfo {
+                name: "Address name".to_string(),
+                logo_uri: Some("logo.url".to_string()),
+            }),
         }),
     };
 
@@ -147,6 +285,38 @@ fn data_decoded_change_implementation_to_settings_info() {
 }
 
 #[test]
+fn data_decoded_change_implementation_to_settings_info_with_address_info() {
+    let mut mock_info_provider = MockInfoProvider::new();
+    mock_info_provider
+        .expect_address_info()
+        .times(1)
+        .return_once(move |_| {
+            Ok(AddressInfo {
+                name: "Address name".to_string(),
+                logo_uri: Some("logo.url".to_string()),
+            })
+        });
+
+    let data_decoded =
+        serde_json::from_str::<DataDecoded>(crate::json::DATA_DECODED_CHANGE_MASTER_COPY).unwrap();
+
+    let expected = SettingsChange {
+        data_decoded: data_decoded.clone(),
+        settings_info: Some(SettingsInfo::ChangeImplementation {
+            implementation: "0xb6029EA3B2c51D09a50B53CA8012FeEB05bDa35A".to_string(),
+            implementation_info: Some(AddressInfo {
+                name: "Address name".to_string(),
+                logo_uri: Some("logo.url".to_string()),
+            }),
+        }),
+    };
+
+    let actual = DataDecoded::to_settings_info(&data_decoded, &mut mock_info_provider);
+
+    assert_eq!(expected.settings_info, actual);
+}
+
+#[test]
 fn data_decoded_enable_module_to_settings_info() {
     let mut mock_info_provider = MockInfoProvider::new();
     mock_info_provider
@@ -171,6 +341,38 @@ fn data_decoded_enable_module_to_settings_info() {
 }
 
 #[test]
+fn data_decoded_enable_module_to_settings_info_with_address_info() {
+    let mut mock_info_provider = MockInfoProvider::new();
+    mock_info_provider
+        .expect_address_info()
+        .times(1)
+        .return_once(move |_| {
+            Ok(AddressInfo {
+                name: "Address name".to_string(),
+                logo_uri: Some("logo.url".to_string()),
+            })
+        });
+
+    let data_decoded =
+        serde_json::from_str::<DataDecoded>(crate::json::DATA_DECODED_ENABLE_MODULE).unwrap();
+
+    let expected = SettingsChange {
+        data_decoded: data_decoded.clone(),
+        settings_info: Some(SettingsInfo::EnableModule {
+            module: "0xF5dC3718EEbC5b003F1672A499F2ACBE77Ba790d".to_string(),
+            module_info: Some(AddressInfo {
+                name: "Address name".to_string(),
+                logo_uri: Some("logo.url".to_string()),
+            }),
+        }),
+    };
+
+    let actual = DataDecoded::to_settings_info(&data_decoded, &mut mock_info_provider);
+
+    assert_eq!(expected.settings_info, actual);
+}
+
+#[test]
 fn data_decoded_disable_module_to_settings_info() {
     let mut mock_info_provider = MockInfoProvider::new();
     mock_info_provider
@@ -186,6 +388,38 @@ fn data_decoded_disable_module_to_settings_info() {
         settings_info: Some(SettingsInfo::DisableModule {
             module: "0x25F73b24B866963B0e560fFF9bbA7908be0263E8".to_string(),
             module_info: None,
+        }),
+    };
+
+    let actual = DataDecoded::to_settings_info(&data_decoded, &mut mock_info_provider);
+
+    assert_eq!(expected.settings_info, actual);
+}
+
+#[test]
+fn data_decoded_disable_module_to_settings_info_with_address_info() {
+    let mut mock_info_provider = MockInfoProvider::new();
+    mock_info_provider
+        .expect_address_info()
+        .times(1)
+        .return_once(move |_| {
+            Ok(AddressInfo {
+                name: "Address name".to_string(),
+                logo_uri: Some("logo.url".to_string()),
+            })
+        });
+
+    let data_decoded =
+        serde_json::from_str::<DataDecoded>(crate::json::DATA_DECODED_DISABLE_MODULE).unwrap();
+
+    let expected = SettingsChange {
+        data_decoded: data_decoded.clone(),
+        settings_info: Some(SettingsInfo::DisableModule {
+            module: "0x25F73b24B866963B0e560fFF9bbA7908be0263E8".to_string(),
+            module_info: Some(AddressInfo {
+                name: "Address name".to_string(),
+                logo_uri: Some("logo.url".to_string()),
+            }),
         }),
     };
 

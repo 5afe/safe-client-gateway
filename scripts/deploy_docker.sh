5abe53250a0e568cddef340834e13a18f21480bf
Jose Alvarez <jp.alvarezl@gmail.com>
2021-02-04 13:39:42 +0100

    Updated the deployment script renamed everything master as main

--- scripts/deploy_docker.sh
+++ scripts/deploy_docker.sh
@@ -2,27 +2,15 @@
 
 set -euo pipefail
 
-if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
-
-    # Use travis build number to identify build
-    export BUILD_NUMBER=$TRAVIS_BUILD_NUMBER
-    # strip the first char as that should always be "v" (as tags should be in the format "vX.X.X")
-    description="$(git describe --tags --always)"
-    export VERSION=${description:1}
-
-    echo "Trigger docker build and upload for version $VERSION ($BUILD_NUMBER)"
-
-    echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-    if [ "$1" = "develop" -o "$1" = "main" ]; then
-        # If image does not exist, don't use cache
-        docker pull $DOCKERHUB_ORG/$DOCKERHUB_PROJECT:$1 && \
-        docker build -t $DOCKERHUB_PROJECT -f Dockerfile --build-arg VERSION --build-arg BUILD_NUMBER . --cache-from $DOCKERHUB_ORG/$DOCKERHUB_PROJECT:$1 || \
-        docker build -t $DOCKERHUB_PROJECT -f Dockerfile --build-arg VERSION --build-arg BUILD_NUMBER .
-    else
-        docker pull $DOCKERHUB_ORG/$DOCKERHUB_PROJECT:staging && \
-        docker build -t $DOCKERHUB_PROJECT -f Dockerfile --build-arg VERSION --build-arg BUILD_NUMBER . --cache-from $DOCKERHUB_ORG/$DOCKERHUB_PROJECT:staging || \
-        docker build -t $DOCKERHUB_PROJECT -f Dockerfile --build-arg VERSION --build-arg BUILD_NUMBER .
-    fi
-    docker tag $DOCKERHUB_PROJECT $DOCKERHUB_ORG/$DOCKERHUB_PROJECT:$1
-    docker push $DOCKERHUB_ORG/$DOCKERHUB_PROJECT:$1
+if [ "$1" = "develop" -o "$1" = "main" ]; then
+    # If image does not exist, don't use cache
+    docker pull gnosispm/$DOCKERHUB_PROJECT:$1 && \
+    docker build -t $DOCKERHUB_PROJECT -f docker/web/Dockerfile . --cache-from gnosispm/$DOCKERHUB_PROJECT:$1 || \
+    docker build -t $DOCKERHUB_PROJECT -f docker/web/Dockerfile .
+else
+    docker pull gnosispm/$DOCKERHUB_PROJECT:staging && \
+    docker build -t $DOCKERHUB_PROJECT -f docker/web/Dockerfile . --cache-from gnosispm/$DOCKERHUB_PROJECT:staging || \
+    docker build -t $DOCKERHUB_PROJECT -f docker/web/Dockerfile .
 fi
+docker tag $DOCKERHUB_PROJECT gnosispm/$DOCKERHUB_PROJECT:$1
+docker push gnosispm/$DOCKERHUB_PROJECT:$1

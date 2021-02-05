c41b61ef29e0af9669ffddb86d6b94d5d87a0d4f
Jose Alvarez <jpalvarezl@users.noreply.github.com>
2021-02-05 18:12:23 +0100

    Fixed path to Dockerfile in the deployment script (#251)

--- scripts/deploy_docker.sh
+++ scripts/deploy_docker.sh
@@ -5,12 +5,12 @@ set -euo pipefail
 if [ "$1" = "develop" -o "$1" = "main" ]; then
     # If image does not exist, don't use cache
     docker pull gnosispm/$DOCKERHUB_PROJECT:$1 && \
-    docker build -t $DOCKERHUB_PROJECT -f docker/web/Dockerfile . --cache-from gnosispm/$DOCKERHUB_PROJECT:$1 || \
-    docker build -t $DOCKERHUB_PROJECT -f docker/web/Dockerfile .
+    docker build -t $DOCKERHUB_PROJECT -f Dockerfile . --cache-from gnosispm/$DOCKERHUB_PROJECT:$1 || \
+    docker build -t $DOCKERHUB_PROJECT -f Dockerfile .
 else
     docker pull gnosispm/$DOCKERHUB_PROJECT:staging && \
-    docker build -t $DOCKERHUB_PROJECT -f docker/web/Dockerfile . --cache-from gnosispm/$DOCKERHUB_PROJECT:staging || \
-    docker build -t $DOCKERHUB_PROJECT -f docker/web/Dockerfile .
+    docker build -t $DOCKERHUB_PROJECT -f Dockerfile . --cache-from gnosispm/$DOCKERHUB_PROJECT:staging || \
+    docker build -t $DOCKERHUB_PROJECT -f Dockerfile .
 fi
 docker tag $DOCKERHUB_PROJECT gnosispm/$DOCKERHUB_PROJECT:$1
 docker push gnosispm/$DOCKERHUB_PROJECT:$1

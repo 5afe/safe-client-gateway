b349bd1be7445d2209de1252aae27ad64860f6f2
Jose Alvarez <jp.alvarezl@gmail.com>
2021-02-08 09:24:18 +0100

    Restored deployment script and adjusted to use GitHub env

--- scripts/deploy_docker.sh
+++ scripts/deploy_docker.sh
@@ -2,15 +2,27 @@
 
 set -euo pipefail
 
-if [ "$1" = "develop" -o "$1" = "main" ]; then
-    # If image does not exist, don't use cache
-    docker pull gnosispm/$DOCKERHUB_PROJECT:$1 && \
-    docker build -t $DOCKERHUB_PROJECT -f Dockerfile . --cache-from gnosispm/$DOCKERHUB_PROJECT:$1 || \
-    docker build -t $DOCKERHUB_PROJECT -f Dockerfile .
-else
-    docker pull gnosispm/$DOCKERHUB_PROJECT:staging && \
-    docker build -t $DOCKERHUB_PROJECT -f Dockerfile . --cache-from gnosispm/$DOCKERHUB_PROJECT:staging || \
-    docker build -t $DOCKERHUB_PROJECT -f Dockerfile .
+if [ "$PULL_REQUEST_NUMBER" = "" ]; then
+
+    # Workflow run number
+    export BUILD_NUMBER=$GITHUB_RUN_NUMBER
+    # strip the first char as that should always be "v" (as tags should be in the format "vX.X.X")
+    description="$(git describe --tags --always)"
+    export VERSION=${description:1}
+
+    echo "Trigger docker build and upload for version $VERSION ($BUILD_NUMBER)"
+
+    echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+    if [ "$1" = "develop" -o "$1" = "main" ]; then
+        # If image does not exist, don't use cache
+        docker pull $DOCKERHUB_ORG/$DOCKERHUB_PROJECT:$1 && \
+        docker build -t $DOCKERHUB_PROJECT -f Dockerfile --build-arg VERSION --build-arg BUILD_NUMBER . --cache-from $DOCKERHUB_ORG/$DOCKERHUB_PROJECT:$1 || \
+        docker build -t $DOCKERHUB_PROJECT -f Dockerfile --build-arg VERSION --build-arg BUILD_NUMBER .
+    else
+        docker pull $DOCKERHUB_ORG/$DOCKERHUB_PROJECT:staging && \
+        docker build -t $DOCKERHUB_PROJECT -f Dockerfile --build-arg VERSION --build-arg BUILD_NUMBER . --cache-from $DOCKERHUB_ORG/$DOCKERHUB_PROJECT:staging || \
+        docker build -t $DOCKERHUB_PROJECT -f Dockerfile --build-arg VERSION --build-arg BUILD_NUMBER .
+    fi
+    docker tag $DOCKERHUB_PROJECT $DOCKERHUB_ORG/$DOCKERHUB_PROJECT:$1
+    docker push $DOCKERHUB_ORG/$DOCKERHUB_PROJECT:$1
 fi
-docker tag $DOCKERHUB_PROJECT gnosispm/$DOCKERHUB_PROJECT:$1
-docker push gnosispm/$DOCKERHUB_PROJECT:$1

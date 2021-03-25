01bf9dc9dd99724cc31a00f36adef9d20635ccc8
Jose Alvarez <jp.alvarezl@gmail.com>
2021-03-25 11:10:37 +0100

    removed csv local loading

--- scripts/load_tester/start.py
+++ scripts/load_tester/start.py
@@ -1,13 +1,13 @@
 import os
 import requests
 import sys
-import csv
 from dotenv import load_dotenv
 
 load_dotenv("../../.env")
 TX_SERVICE_URL = os.getenv("TRANSACTION_SERVICE_URL")
 PRINT_FORMAT = "{0:<10} {1:>8}::{2:>8}"
 SAFES_CSV_FILE_NAME = 'safes.csv'
+
 
 def check_service():
     local_instance_check = requests.get("http://localhost:8000/about")
@@ -16,42 +16,20 @@ def check_service():
         sys.exit(-1)
 
 
-def load_safes_from_file() -> list[str]:
-    print("Loading from file...")
-    with open(SAFES_CSV_FILE_NAME) as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',', quotechar='"')
-        next(csv_file)  # skip header name
-
-        output = []
-        for row in csv_reader:
-            output.append(row[0])
-        return output
-
-
-def load_safes_remote() -> list[str]:
+def load_safes() -> list[str]:
     print("Loading remote ...")
     top_safes_url = TX_SERVICE_URL + "/api/v1/analytics/multisig-transactions/by-safe/?limit=300"
     print(top_safes_url)
     response = requests.get(top_safes_url)
-    safes = list(map(lambda safe: safe['safe'], response.json()['results']))
-    with open(SAFES_CSV_FILE_NAME, 'w') as myfile:
-        wr = csv.writer(myfile, delimiter=",", quoting=csv.QUOTE_ALL)
-        wr.writerow(['safe_address'])
-        for safe in safes:
-            wr.writerow([safe])
-    return safes
-
-
-def load_safes() -> list[str]:
-    return load_safes_from_file() if os.path.isfile(SAFES_CSV_FILE_NAME) else load_safes_remote()
+    return list(map(lambda safe: safe['safe'], response.json()['results']))
 
 
 check_service()
 safes = load_safes()
 print("Top 300 safes:")
 print("\n\t" + "\n\t".join(safes))
-print("Safes ready for tests ... ")
-print("Starting tests ...")
+print("Safes ready ... ")
+print("Populating cache ...")
 
 for safe in safes:
     balance_response = requests.get("http://localhost:8000/v1/safes/" + safe + "/balances/USD")

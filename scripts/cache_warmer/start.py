30ff38ff39a33b4f8646c1656d7874759b349f8d
Jose Alvarez <jpalvarezl@users.noreply.github.com>
2021-03-29 08:54:14 +0200

    Script to populate cache (#320)
    * Works but there is no progress console output
    
    * Fetched list of top 300 safes from analytics endpoint
    
    * WIP
    
    * Added documentation and dependencies file
    
    * Added installation of deps and usage of virtual envs for the script, updated documentation
    
    * Added early exit for when the service is not running
    
    * Added caching of the safes
    
    * Installing drill is part of the script
    
    * Removed asyncio dep
    
    * Added first benchmark
    
    * Added more console messages
    
    * Removed unnecessary trailing slash
    
    * Added requests to other endpoints
    
    * Added instructions to run the script
    
    * Removed the usage of drill
    
    * removed csv local loading
    
    * Removed csv file pattern from .gitignore
    
    * Added hard check for only staging and base URL for staging networks for the gateway
    
    * Removed check for service environment
    
    * Comments from PR
    
    * Renamed and refactored script

---
+++ scripts/cache_warmer/start.py
@@ -0,0 +1,47 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv("../../.env")
+TX_SERVICE_URL = os.getenv("TRANSACTION_SERVICE_URL")
+PRINT_FORMAT = "{0:<10} {1:>8}::{2:>8}"
+
+
+def get_base_url() -> str:
+    if "staging" not in TX_SERVICE_URL:
+        return "https://safe-client.rinkeby.gnosis.io" if "rinkeby" in TX_SERVICE_URL \
+            else "https://safe-client.mainnet.gnosis.io"
+    else:
+        return "https://safe-client-rinkeby.staging.gnosisdev.com" if "rinkeby" in TX_SERVICE_URL \
+            else "https://safe-client-mainnet.staging.gnosisdev.com"
+
+
+def load_safes() -> list[str]:
+    print("Loading remote ...")
+    top_safes_url = TX_SERVICE_URL + "/api/v1/analytics/multisig-transactions/by-safe/?limit=300"
+    print(top_safes_url)
+    response = requests.get(top_safes_url)
+    return list(map(lambda safe: safe['safe'], response.json()['results']))
+
+
+def warm_up_safes(base_gateway_url: str, safes: list[str]):
+    for safe in safes:
+        balance_response = requests.get("%s/v1/safes/%s/balances/USD" % (base_gateway_url, safe))
+        collectibles_response = requests.get("%s/v1/safes/%s/collectibles" % (base_gateway_url, safe))
+        tx_queued_response = requests.get("%s/v1/safes/%s/transactions/queued" % (base_gateway_url, safe))
+        tx_history_response = requests.get("%s/v1/safes/%s/transactions/history" % (base_gateway_url, safe))
+
+        print(PRINT_FORMAT.format(str(balance_response.elapsed.total_seconds()), str(
+            balance_response.status_code), balance_response.url))
+        print(PRINT_FORMAT.format(str(collectibles_response.elapsed.total_seconds()), str(
+            collectibles_response.status_code), collectibles_response.url))
+        print(PRINT_FORMAT.format(str(tx_queued_response.elapsed.total_seconds()), str(
+            tx_queued_response.status_code), tx_queued_response.url))
+        print(PRINT_FORMAT.format(str(tx_history_response.elapsed.total_seconds()), str(
+            tx_history_response.status_code), tx_history_response.url))
+        print()
+
+
+url = get_base_url()
+safes_list = load_safes()
+warm_up_safes(base_gateway_url=url, safes=safes_list)

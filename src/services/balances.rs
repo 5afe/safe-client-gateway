88039a07a1f4fbe0a692cc6c5c7f48f45a5c8543
Jose Alvarez <jpalvarezl@users.noreply.github.com>
2021-04-01 11:34:03 +0200

    Added API key and extra step to avoid assumptions on the base currenc… (#334)
    * Added API key and extra step to avoid assumptions on the base currency returned
    
    * rustfmt
    
    * Version bump

--- src/services/balances.rs
+++ src/services/balances.rs
@@ -33,7 +33,7 @@ pub fn balances(
     let backend_balances: Vec<BalanceDto> = serde_json::from_str(&body)?;
 
     let info_provider = DefaultInfoProvider::new(&context);
-    let usd_to_fiat = info_provider.exchange_usd_to(fiat)?;
+    let usd_to_fiat = info_provider.exchange_usd_to(fiat).unwrap_or(0.0);
 
     let mut total_fiat = 0.0;
 

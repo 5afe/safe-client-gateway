1ff9656973be79b48980a4a20d887d82c4a03270
Jose Alvarez <jp.alvarezl@gmail.com>
2021-04-12 16:19:38 +0200

    Inlined operator specific method for requested_cache

--- src/cache/cache_op_executors.rs
+++ src/cache/cache_op_executors.rs
@@ -33,15 +33,55 @@ pub(super) fn request_cached(
     client: &reqwest::blocking::Client,
     operation: &RequestCached,
 ) -> ApiResult<String> {
-    request_cached_priv(
-        cache,
-        client,
-        &operation.url,
-        operation.cache_duration,
-        operation.error_cache_duration,
-        operation.cache_all_errors,
-        operation.request_timeout,
-    )
+    let cache_key = format!("{}_{}", CACHE_REQS_PREFIX, &operation.url);
+    match cache.fetch(&cache_key) {
+        Some(cached) => CachedWithCode::split(&cached).to_result(),
+        None => {
+            let mut request = client.get(&operation.url);
+            if operation.request_timeout > 0 {
+                request = request.timeout(Duration::from_millis(operation.request_timeout));
+            }
+            let response = request.send().map_err(|err| {
+                if operation.cache_all_errors {
+                    cache.create(
+                        &cache_key,
+                        &CachedWithCode::join(500, &format!("{:?}", &err)),
+                        operation.error_cache_duration,
+                    );
+                }
+                err
+            })?;
+            let status_code = response.status().as_u16();
+
+            // Early return and no caching if the error is a 500 or greater
+            let is_server_error = response.status().is_server_error();
+            if !operation.cache_all_errors && is_server_error {
+                return Err(ApiError::from_backend_error(
+                    42,
+                    &format!("Got server error for {}", response.text()?),
+                ));
+            }
+
+            let is_client_error = response.status().is_client_error();
+            let raw_data = response.text()?;
+
+            if is_client_error || is_server_error {
+                cache.create(
+                    &cache_key,
+                    &CachedWithCode::join(status_code, &raw_data),
+                    operation.error_cache_duration,
+                );
+                Err(ApiError::from_backend_error(status_code, &raw_data))
+            } else {
+                cache.create(
+                    &cache_key,
+                    &CachedWithCode::join(status_code, &raw_data),
+                    operation.cache_duration,
+                );
+                Ok(raw_data.to_string())
+            }
+        }
+    }
 }
 
 fn request_cached_priv(

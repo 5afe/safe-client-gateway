549aebdf432ef56d8f9c72137491216f9fdb4e22
Jose Alvarez <jp.alvarezl@gmail.com>
2021-04-12 12:14:43 +0200

    Restored invalidation pattern for webhook trigger

--- src/services/hooks.rs
+++ src/services/hooks.rs
@@ -5,22 +5,22 @@ use crate::utils::errors::ApiResult;
 
 pub fn invalidate_caches(cache: &impl Cache, payload: &Payload) -> ApiResult<()> {
     Invalidate::new()
-        .pattern(format!("*{}*", &payload.address))
+        .pattern(format!("c_re*{}*", &payload.address))
         .execute(cache);
     payload.details.as_ref().map(|d| match d {
         PayloadDetails::NewConfirmation(data) => {
             Invalidate::new()
-                .pattern(format!("*{}*", &data.safe_tx_hash))
+                .pattern(format!("c_re*{}*", &data.safe_tx_hash))
                 .execute(cache);
         }
         PayloadDetails::ExecutedMultisigTransaction(data) => {
             Invalidate::new()
-                .pattern(format!("*{}*", &data.safe_tx_hash))
+                .pattern(format!("c_re*{}*", &data.safe_tx_hash))
                 .execute(cache);
         }
         PayloadDetails::PendingMultisigTransaction(data) => {
             Invalidate::new()
-                .pattern(format!("*{}*", &data.safe_tx_hash))
+                .pattern(format!("c_re*{}*", &data.safe_tx_hash))
                 .execute(cache);
         }
         _ => {}

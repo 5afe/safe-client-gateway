79448facea480ff731aedd45a43f0191032d0c9e
Jose Alvarez <jp.alvarezl@gmail.com>
2021-04-28 15:57:09 +0200

    Added structures to accommodate for timestamps

--- src/models/service/safes.rs
+++ src/models/service/safes.rs
@@ -22,3 +22,23 @@ pub struct AddressEx {
     #[serde(skip_serializing_if = "Option::is_none")]
     pub logo_url: Option<String>,
 }
+
+#[derive(Serialize, Debug, PartialEq)]
+#[serde(rename_all = "camelCase")]
+pub struct SafeLastChanges {
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub collectibles: Option<i64>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub tx_queued: Option<i64>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub tx_history: Option<i64>,
+}
+
+#[derive(Serialize, Debug, PartialEq)]
+#[serde(rename_all = "camelCase")]
+pub struct SafeState {
+    #[serde(flatten)]
+    pub safe_config: SafeInfoEx,
+    #[serde(flatten)]
+    pub safe_state: SafeLastChanges,
+}
--- src/services/safes.rs
+++ src/services/safes.rs
@@ -1,11 +1,22 @@
-use crate::models::service::safes::SafeInfoEx;
+use crate::models::service::safes::{SafeInfoEx, SafeLastChanges, SafeState};
 use crate::providers::info::{DefaultInfoProvider, InfoProvider};
 use crate::utils::context::Context;
 use crate::utils::errors::ApiResult;
 
-pub fn get_safe_info_ex(context: &Context, safe_address: &String) -> ApiResult<SafeInfoEx> {
+pub fn get_safe_info_ex(context: &Context, safe_address: &String) -> ApiResult<SafeState> {
     let mut info_provider = DefaultInfoProvider::new(context);
     let safe_info = info_provider.safe_info(safe_address)?;
 
-    Ok(safe_info.to_safe_info_ex(&mut info_provider))
+    let safe_info_ex = safe_info.to_safe_info_ex(&mut info_provider);
+
+    let safe_state = SafeState {
+        safe_config: safe_info_ex,
+        safe_state: SafeLastChanges {
+            collectibles: None,
+            tx_queued: None,
+            tx_history: None,
+        },
+    };
+
+    Ok(safe_state)
 }

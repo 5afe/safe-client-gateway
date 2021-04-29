fbd7ca6e6506299922926e34c38817f0aeea53d5
Jose Alvarez <jp.alvarezl@gmail.com>
2021-04-29 12:41:58 +0200

    Added method for tx_history latest timestamp

--- src/services/safes.rs
+++ src/services/safes.rs
@@ -1,11 +1,13 @@
 use crate::cache::cache_operations::RequestCached;
 use crate::config::{base_transaction_service_url, transaction_request_timeout};
-use crate::models::backend::transactions::MultisigTransaction;
+use crate::models::backend::transactions::{MultisigTransaction, Transaction};
 use crate::models::commons::Page;
 use crate::models::service::safes::{SafeInfoEx, SafeLastChanges, SafeState};
+use crate::models::service::transactions::summary::TransactionSummary;
 use crate::providers::info::{DefaultInfoProvider, InfoProvider};
 use crate::utils::context::Context;
 use crate::utils::errors::ApiResult;
+use chrono::{DateTime, TimeZone, Utc};
 
 pub fn get_safe_info_ex(context: &Context, safe_address: &String) -> ApiResult<SafeState> {
     let mut info_provider = DefaultInfoProvider::new(context);
@@ -20,7 +22,7 @@ pub fn get_safe_info_ex(context: &Context, safe_address: &String) -> ApiResult<S
         safe_state: SafeLastChanges {
             collectibles: 0,
             tx_queued: get_last_queued_tx(context, safe_address).unwrap_or(0),
-            tx_history: 0,
+            tx_history: get_last_history_tx(context, safe_address).unwrap_or(0),
         },
     };
 
@@ -49,5 +51,38 @@ fn get_last_queued_tx(context: &Context, safe_address: &String) -> ApiResult<i64
         .get(0)
         .as_ref()
         .map(|tx| tx.submission_date.timestamp())
+        .ok_or(api_error!("Couldn't get tx timestamps"))
+}
+
+fn get_last_history_tx(context: &Context, safe_address: &String) -> ApiResult<i64> {
+    let mut info_provider = DefaultInfoProvider::new(context);
+
+    let url = format!(
+        "{}/v1/safes/{}/all-transactions/?\
+        &queued=false\
+        &executed=true",
+        base_transaction_service_url(),
+        safe_address
+    );
+
+    let body = RequestCached::new(url)
+        .request_timeout(transaction_request_timeout())
+        .execute(context.client(), context.cache())?;
+    let transaction: Page<Transaction> = serde_json::from_str(&body)?;
+
+    log::debug!("{:#?}", &transaction);
+    transaction
+        .results
+        .get(0)
+        .as_ref()
+        .and_then(|tx| {
+            tx.to_transaction_summary(&mut info_provider, safe_address)
+                .ok()
+                .as_ref()
+                .map(|txs| txs.get(0))
+                .flatten()
+                .as_ref()
+                .map(|tx| tx.timestamp / 1000)
+        })
         .ok_or(api_error!("Couldn't get tx timestamps"))
 }

317cb509cb768d8487d241b44e1bf72f5dbf0729
Jose Alvarez <jpalvarezl@users.noreply.github.com>
2021-04-29 15:15:48 +0200

    Added Tokens variant to invalidation patterns (#360)

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -31,6 +31,7 @@ pub fn active_routes() -> Vec<Route> {
         transactions::propose_transaction,
         hooks::update,
         hooks::flush_all,
+        hooks::flush_token_info,
         health::health
     ]
 }

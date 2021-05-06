6ff22184803df0a080a402831f2b89eac2b002cf
Jose Alvarez <jp.alvarezl@gmail.com>
2021-05-06 15:24:48 +0200

    Added support to spectrum in the cache warmer script

--- scripts/cache_warmer/start.py
+++ scripts/cache_warmer/start.py
@@ -1,5 +1,6 @@
 import os
 import requests
+import sys, getopt
 from dotenv import load_dotenv
 
 load_dotenv("../../.env")
@@ -7,8 +8,10 @@ TX_SERVICE_URL = os.getenv("TRANSACTION_SERVICE_URL")
 PRINT_FORMAT = "{0:<10} {1:>8}::{2:>8}"
 
 
-def get_base_url() -> str:
-    if "staging" not in TX_SERVICE_URL:
+def get_base_url(is_spectrum: bool) -> str:
+    if is_spectrum:
+        return "https://safe-client-spectrum.mainnet.staging.gnosisdev.com"
+    elif "staging" not in TX_SERVICE_URL:
         return "https://safe-client.rinkeby.gnosis.io" if "rinkeby" in TX_SERVICE_URL \
             else "https://safe-client.mainnet.gnosis.io"
     else:
@@ -16,9 +19,10 @@ def get_base_url() -> str:
             else "https://safe-client-mainnet.staging.gnosisdev.com"
 
 
-def load_safes() -> list[str]:
+def load_safes(is_spectrum: bool) -> list[str]:
+    tx_service_url = "https://safe-transaction.mainnet.staging.gnosisdev.com" if is_spectrum else TX_SERVICE_URL
     print("Loading remote ...")
-    top_safes_url = TX_SERVICE_URL + "/api/v1/analytics/multisig-transactions/by-safe/?limit=300"
+    top_safes_url = tx_service_url + "/api/v1/analytics/multisig-transactions/by-safe/?limit=300"
     print(top_safes_url)
     response = requests.get(top_safes_url)
     return list(map(lambda safe: safe['safe'], response.json()['results']))
@@ -42,6 +46,7 @@ def warm_up_safes(base_gateway_url: str, safes: list[str]):
         print()
 
 
-url = get_base_url()
-safes_list = load_safes()
+is_spectrum = len(sys.argv) > 1
+url = get_base_url(is_spectrum)
+safes_list = load_safes(is_spectrum)
 warm_up_safes(base_gateway_url=url, safes=safes_list)

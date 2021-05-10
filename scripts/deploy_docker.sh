c0cc2281ab36848305ed55227a0d76689f62340b
Richard Meissner <msc.meissner@gmail.com>
2021-05-10 22:39:08 +0200

    Try to better cache multi-stage docker build

--- scripts/deploy_docker.sh
+++ scripts/deploy_docker.sh
@@ -11,14 +11,36 @@ export VERSION=${description:1}
 echo "Trigger docker build and upload for version $VERSION ($BUILD_NUMBER)"
 
 if [ "$1" = "develop" -o "$1" = "main" -o "$1" = "spectrum" ]; then
-    # If image does not exist, don't use cache
-    docker pull $DOCKERHUB_ORG/$DOCKERHUB_PROJECT:$1 && \
-    docker build -t $DOCKERHUB_PROJECT -f Dockerfile --build-arg VERSION --build-arg BUILD_NUMBER . --cache-from $DOCKERHUB_ORG/$DOCKERHUB_PROJECT:$1 || \
-    docker build -t $DOCKERHUB_PROJECT -f Dockerfile --build-arg VERSION --build-arg BUILD_NUMBER .
+    cache_tag="$1"
 else
-    docker pull $DOCKERHUB_ORG/$DOCKERHUB_PROJECT:staging && \
-    docker build -t $DOCKERHUB_PROJECT -f Dockerfile --build-arg VERSION --build-arg BUILD_NUMBER . --cache-from $DOCKERHUB_ORG/$DOCKERHUB_PROJECT:staging || \
-    docker build -t $DOCKERHUB_PROJECT -f Dockerfile --build-arg VERSION --build-arg BUILD_NUMBER .
+    cache_tag="staging"
 fi
+
+# Load cached builder image
+docker pull $DOCKERHUB_ORG/$DOCKERHUB_PROJECT:$cache_tag-builder || true
+# Rebuild builder image if required
+docker build \
+    --target builder \
+    --cache-from $DOCKERHUB_ORG/$DOCKERHUB_PROJECT:$cache_tag-builder \
+    -t $DOCKERHUB_PROJECT \
+    -f Dockerfile \
+    --build-arg VERSION --build-arg BUILD_NUMBER \
+    .
+
+# Load cached runtime image
+docker pull $DOCKERHUB_ORG/$DOCKERHUB_PROJECT:$cache_tag || true
+# Rebuild runtime image if required
+docker build \
+    --cache-from $DOCKERHUB_ORG/$DOCKERHUB_PROJECT:$cache_tag-builder \
+    --cache-from $DOCKERHUB_ORG/$DOCKERHUB_PROJECT:$cache_tag \
+    -t $DOCKERHUB_PROJECT \
+    -f Dockerfile \
+    --build-arg VERSION --build-arg BUILD_NUMBER \
+    .
+
+# Push runtime images to remote repository
 docker tag $DOCKERHUB_PROJECT $DOCKERHUB_ORG/$DOCKERHUB_PROJECT:$1
 docker push $DOCKERHUB_ORG/$DOCKERHUB_PROJECT:$1
+
+# Push builder image to remote repository for next build
+docker push $DOCKERHUB_ORG/$DOCKERHUB_PROJECT:$cache_tag-builder

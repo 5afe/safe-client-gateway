1336bc409c042a1e5f318c0e8bd680f257ee0927
Jose Alvarez <jp.alvarezl@gmail.com>
2021-05-12 14:53:01 +0200

    Fixed full address info search method

--- src/providers/info.rs
+++ src/providers/info.rs
@@ -161,11 +161,14 @@ impl<C: Cache> InfoProvider for DefaultInfoProvider<'_, C> {
     }
 
     async fn full_address_info_search(&self, address: &str) -> ApiResult<AddressInfo> {
-        self.token_info(&address).await.map(|it| AddressInfo {
+        let token_info = self.token_info(&address).await.map(|it| AddressInfo {
             name: it.name,
             logo_uri: it.logo_uri.to_owned(),
-        })?;
-        self.contract_info(&address).await
+        });
+        match token_info {
+            Ok(_) => token_info,
+            Err(_) => self.contract_info(&address).await,
+        }
     }
 }
 

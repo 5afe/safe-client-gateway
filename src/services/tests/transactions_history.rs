83c4b78a7d2c7c07489e97ef2fa5f1c4124f9d71
fmrsabino <3332770+fmrsabino@users.noreply.github.com>
2021-05-20 12:19:48 +0200

    Fix existing tests

--- src/services/tests/transactions_history.rs
+++ src/services/tests/transactions_history.rs
@@ -70,7 +70,10 @@ async fn backend_txs_to_summary_txs_with_values() {
         serde_json::from_str::<Page<Transaction>>(BACKEND_HISTORY_TRANSACTION_LIST_PAGE).unwrap();
     let mut mock_info_provider = MockInfoProvider::new();
     mock_info_provider.expect_safe_info().times(0);
-    mock_info_provider.expect_token_info().times(0);
+    mock_info_provider
+        .expect_token_info()
+        .times(3)
+        .returning(move |_| bail!("No token info"));
     mock_info_provider
         .expect_full_address_info_search()
         .times(6)
@@ -78,161 +81,161 @@ async fn backend_txs_to_summary_txs_with_values() {
 
     let mut back_end_txs_iter = backend_txs.results.into_iter();
     let expected = vec![
-     TransactionSummary {
-         id: "module_0x1230B3d59858296A31053C1b8562Ecf89A2f888b_0xcd10b23687bf336d0f4c0a3383590d3d1722aaa99a41fd0d289a5f69a8266c8f_0x9b803d5301fcea17".into(),
-         timestamp: 1606845854000,
-         tx_status: Success,
-         tx_info: TransactionInfo::Custom(
-             Custom {
-                 to: "0xc778417E063141139Fce010982780140Aa0cD5Ab".into(),
-                 data_size: "68".into(),
-                 value: "0".into(),
-                 method_name: Some("transfer".into()),
-                 action_count: None,
-                 to_info: None,
-                 is_cancellation: false
-             },
-         ),
-         execution_info: None,
-         safe_app_info: None,
-     },
-     TransactionSummary {
-         id: "module_0x1230B3d59858296A31053C1b8562Ecf89A2f888b_0x1cf24abdb39bb7b156677a128e709cea55c6991b12708904d1f0f3664ad6646e_0x1aa17c7f85908be0".into(),
-         timestamp: 1606845794000,
-         tx_status: Success,
-         tx_info: TransactionInfo::Custom(
-             Custom {
-                 to: "0xD9BA894E0097f8cC2BBc9D24D308b98e36dc6D02".into(),
-                 data_size: "68".into(),
-                 value: "0".into(),
-                 method_name: Some("transfer".into()),
-                 action_count: None,
-                 to_info: None,
-                 is_cancellation: false
-             },
-         ),
-         execution_info: None,
-         safe_app_info: None,
-     },
-     TransactionSummary {
-         id: "module_0x1230B3d59858296A31053C1b8562Ecf89A2f888b_0x3f12bb74cd91ef09d553f66e3623bceaf879ba3dcb325227b1fbf2455757891a_0xc2c2c8177187ac6c".into(),
-         timestamp: 1606845070000,
-         tx_status: Success,
-         tx_info: TransactionInfo::Custom(
-             Custom {
-                 to: "0xD9BA894E0097f8cC2BBc9D24D308b98e36dc6D02".into(),
-                 data_size: "68".into(),
-                 value: "0".into(),
-                 method_name: Some("transfer".into()),
-                 action_count: None,
-                 to_info: None,
-                 is_cancellation: false
-             },
-         ),
-         execution_info: None,
-         safe_app_info: None,
-     },
-     TransactionSummary {
-         id: "ethereum_0x1230B3d59858296A31053C1b8562Ecf89A2f888b_0x021d4d8cb68f3f772906b58f97b66c6ead228c252627c5b1aff4b496d4ff0c2d_0xfd0dbbc7700a140f".into(),
-         timestamp: 1606744033000,
-         tx_status: Success,
-         tx_info: TransactionInfo::Transfer(
-             Transfer {
-                 sender: "0x1230B3d59858296A31053C1b8562Ecf89A2f888b".into(),
-                 sender_info: None,
-                 recipient: "0xF353eBBa77e5E71c210599236686D51cA1F88b84".into(),
-                 recipient_info: None,
-                 direction: Outgoing,
-                 transfer_info:  TransferInfo::Erc20(
-                     Erc20Transfer {
-                         token_address: "0x63704B63Ac04f3a173Dfe677C7e3D330c347CD88".into(),
-                         token_name: Some(
-                             "TEST AQER".into(),
-                         ),
-                         token_symbol: Some(
-                             "AQER".into(),
-                         ),
-                         logo_uri: Some(
-                             "https://gnosis-safe-token-logos.s3.amazonaws.com/0x63704B63Ac04f3a173Dfe677C7e3D330c347CD88.png".into(),
-                         ),
-                         decimals: Some(
-                             18,
-                         ),
-                         value: "100000000000000000".into(),
-                     },
-                 ),
-             },
-         ),
-         execution_info: None,
-         safe_app_info: None,
-     },
-     TransactionSummary {
-         id: "ethereum_0x1230B3d59858296A31053C1b8562Ecf89A2f888b_0x5f4b7555f8e977ae302ab4125de685ccfacf52ac70e6f0aa2939bcb347f9a732_0xb7ceaac0cd5a85c5".into(),
-         timestamp: 1606743581000,
-         tx_status: Success,
-         tx_info: TransactionInfo::Transfer(
-             Transfer {
-                 sender: "0x1230B3d59858296A31053C1b8562Ecf89A2f888b".into(),
-                 sender_info: None,
-                 recipient: "0xf2565317F3Ae8Ae9EA98E9Fe1e7FADC77F823cbD".into(),
-                 recipient_info: None,
-                 direction: Outgoing,
-                 transfer_info:  TransferInfo::Erc20(
-                     Erc20Transfer {
-                         token_address: "0x63704B63Ac04f3a173Dfe677C7e3D330c347CD88".into(),
-                         token_name: Some(
-                             "TEST AQER".into(),
-                         ),
-                         token_symbol: Some(
-                             "AQER".into(),
-                         ),
-                         logo_uri: Some(
-                             "https://gnosis-safe-token-logos.s3.amazonaws.com/0x63704B63Ac04f3a173Dfe677C7e3D330c347CD88.png".into(),
-                         ),
-                         decimals: Some(18),
-                         value: "100000000000000000".into(),
-                     },
-                 ),
-             },
-         ),
-         execution_info: None,
-         safe_app_info: None,
-     },
-     TransactionSummary {
-         id: "ethereum_0x1230B3d59858296A31053C1b8562Ecf89A2f888b_0xaafed95936f9d71eb8d9612e83f3f93f9decf33f11bbb4aa79cae98966ffa7fe_0x11bd3d64559a0af7".into(),
-         timestamp: 1606739725000,
-         tx_status: Success,
-         tx_info: TransactionInfo::Transfer(
-             Transfer {
-                 sender: "0xf2565317F3Ae8Ae9EA98E9Fe1e7FADC77F823cbD".into(),
-                 sender_info: None,
-                 recipient: "0x1230B3d59858296A31053C1b8562Ecf89A2f888b".into(),
-                 recipient_info: None,
-                 direction: Incoming,
-                 transfer_info: TransferInfo::Erc20(
-                     Erc20Transfer {
-                         token_address: "0x81D0FF4fE216fB6aC98ED609086A92d94dbfE666".into(),
-                         token_name: Some(
-                             "LS".into(),
-                         ),
-                         token_symbol: Some(
-                             "LS".into(),
-                         ),
-                         logo_uri: Some(
-                             "https://gnosis-safe-token-logos.s3.amazonaws.com/0x81D0FF4fE216fB6aC98ED609086A92d94dbfE666.png".into(),
-                         ),
-                         decimals: Some(
-                             18,
-                         ),
-                         value: "400000000000000".into(),
-                     },
-                 ),
-             },
-         ),
-         execution_info: None,
-         safe_app_info: None,
-     },
- ];
+        TransactionSummary {
+            id: "module_0x1230B3d59858296A31053C1b8562Ecf89A2f888b_0xcd10b23687bf336d0f4c0a3383590d3d1722aaa99a41fd0d289a5f69a8266c8f_0xd53ac0f689ace74a".into(),
+            timestamp: 1606845854000,
+            tx_status: Success,
+            tx_info: TransactionInfo::Custom(
+                Custom {
+                    to: "0xc778417E063141139Fce010982780140Aa0cD5Ab".into(),
+                    data_size: "68".into(),
+                    value: "0".into(),
+                    method_name: Some("transfer".into()),
+                    action_count: None,
+                    to_info: None,
+                    is_cancellation: false,
+                },
+            ),
+            execution_info: None,
+            safe_app_info: None,
+        },
+        TransactionSummary {
+            id: "module_0x1230B3d59858296A31053C1b8562Ecf89A2f888b_0x1cf24abdb39bb7b156677a128e709cea55c6991b12708904d1f0f3664ad6646e_0x6000265bdbf4bceb".into(),
+            timestamp: 1606845794000,
+            tx_status: Success,
+            tx_info: TransactionInfo::Custom(
+                Custom {
+                    to: "0xD9BA894E0097f8cC2BBc9D24D308b98e36dc6D02".into(),
+                    data_size: "68".into(),
+                    value: "0".into(),
+                    method_name: Some("transfer".into()),
+                    action_count: None,
+                    to_info: None,
+                    is_cancellation: false,
+                },
+            ),
+            execution_info: None,
+            safe_app_info: None,
+        },
+        TransactionSummary {
+            id: "module_0x1230B3d59858296A31053C1b8562Ecf89A2f888b_0x3f12bb74cd91ef09d553f66e3623bceaf879ba3dcb325227b1fbf2455757891a_0x8a20bf269d0c3ee2".into(),
+            timestamp: 1606845070000,
+            tx_status: Success,
+            tx_info: TransactionInfo::Custom(
+                Custom {
+                    to: "0xD9BA894E0097f8cC2BBc9D24D308b98e36dc6D02".into(),
+                    data_size: "68".into(),
+                    value: "0".into(),
+                    method_name: Some("transfer".into()),
+                    action_count: None,
+                    to_info: None,
+                    is_cancellation: false,
+                },
+            ),
+            execution_info: None,
+            safe_app_info: None,
+        },
+        TransactionSummary {
+            id: "ethereum_0x1230B3d59858296A31053C1b8562Ecf89A2f888b_0x021d4d8cb68f3f772906b58f97b66c6ead228c252627c5b1aff4b496d4ff0c2d_0xfd0dbbc7700a140f".into(),
+            timestamp: 1606744033000,
+            tx_status: Success,
+            tx_info: TransactionInfo::Transfer(
+                Transfer {
+                    sender: "0x1230B3d59858296A31053C1b8562Ecf89A2f888b".into(),
+                    sender_info: None,
+                    recipient: "0xF353eBBa77e5E71c210599236686D51cA1F88b84".into(),
+                    recipient_info: None,
+                    direction: Outgoing,
+                    transfer_info: TransferInfo::Erc20(
+                        Erc20Transfer {
+                            token_address: "0x63704B63Ac04f3a173Dfe677C7e3D330c347CD88".into(),
+                            token_name: Some(
+                                "TEST AQER".into(),
+                            ),
+                            token_symbol: Some(
+                                "AQER".into(),
+                            ),
+                            logo_uri: Some(
+                                "https://gnosis-safe-token-logos.s3.amazonaws.com/0x63704B63Ac04f3a173Dfe677C7e3D330c347CD88.png".into(),
+                            ),
+                            decimals: Some(
+                                18,
+                            ),
+                            value: "100000000000000000".into(),
+                        },
+                    ),
+                },
+            ),
+            execution_info: None,
+            safe_app_info: None,
+        },
+        TransactionSummary {
+            id: "ethereum_0x1230B3d59858296A31053C1b8562Ecf89A2f888b_0x5f4b7555f8e977ae302ab4125de685ccfacf52ac70e6f0aa2939bcb347f9a732_0xb7ceaac0cd5a85c5".into(),
+            timestamp: 1606743581000,
+            tx_status: Success,
+            tx_info: TransactionInfo::Transfer(
+                Transfer {
+                    sender: "0x1230B3d59858296A31053C1b8562Ecf89A2f888b".into(),
+                    sender_info: None,
+                    recipient: "0xf2565317F3Ae8Ae9EA98E9Fe1e7FADC77F823cbD".into(),
+                    recipient_info: None,
+                    direction: Outgoing,
+                    transfer_info: TransferInfo::Erc20(
+                        Erc20Transfer {
+                            token_address: "0x63704B63Ac04f3a173Dfe677C7e3D330c347CD88".into(),
+                            token_name: Some(
+                                "TEST AQER".into(),
+                            ),
+                            token_symbol: Some(
+                                "AQER".into(),
+                            ),
+                            logo_uri: Some(
+                                "https://gnosis-safe-token-logos.s3.amazonaws.com/0x63704B63Ac04f3a173Dfe677C7e3D330c347CD88.png".into(),
+                            ),
+                            decimals: Some(18),
+                            value: "100000000000000000".into(),
+                        },
+                    ),
+                },
+            ),
+            execution_info: None,
+            safe_app_info: None,
+        },
+        TransactionSummary {
+            id: "ethereum_0x1230B3d59858296A31053C1b8562Ecf89A2f888b_0xaafed95936f9d71eb8d9612e83f3f93f9decf33f11bbb4aa79cae98966ffa7fe_0x11bd3d64559a0af7".into(),
+            timestamp: 1606739725000,
+            tx_status: Success,
+            tx_info: TransactionInfo::Transfer(
+                Transfer {
+                    sender: "0xf2565317F3Ae8Ae9EA98E9Fe1e7FADC77F823cbD".into(),
+                    sender_info: None,
+                    recipient: "0x1230B3d59858296A31053C1b8562Ecf89A2f888b".into(),
+                    recipient_info: None,
+                    direction: Incoming,
+                    transfer_info: TransferInfo::Erc20(
+                        Erc20Transfer {
+                            token_address: "0x81D0FF4fE216fB6aC98ED609086A92d94dbfE666".into(),
+                            token_name: Some(
+                                "LS".into(),
+                            ),
+                            token_symbol: Some(
+                                "LS".into(),
+                            ),
+                            logo_uri: Some(
+                                "https://gnosis-safe-token-logos.s3.amazonaws.com/0x81D0FF4fE216fB6aC98ED609086A92d94dbfE666.png".into(),
+                            ),
+                            decimals: Some(
+                                18,
+                            ),
+                            value: "400000000000000".into(),
+                        },
+                    ),
+                },
+            ),
+            execution_info: None,
+            safe_app_info: None,
+        },
+    ];
     let actual = backend_txs_to_summary_txs(
         &mut back_end_txs_iter,
         &mut mock_info_provider,
@@ -258,7 +261,10 @@ fn service_txs_to_tx_list_items_empty() {
 async fn service_txs_to_tx_list_items_last_timestamp_undefined() {
     let mut mock_info_provider = MockInfoProvider::new();
     mock_info_provider.expect_safe_info().times(0);
-    mock_info_provider.expect_token_info().times(0);
+    mock_info_provider
+        .expect_token_info()
+        .times(6)
+        .returning(move |_| bail!("No token info"));
     mock_info_provider
         .expect_full_address_info_search()
         .times(12)
@@ -314,7 +320,10 @@ async fn service_txs_to_tx_list_items_last_timestamp_defined_but_different() {
 
     let mut mock_info_provider = MockInfoProvider::new();
     mock_info_provider.expect_safe_info().times(0);
-    mock_info_provider.expect_token_info().times(0);
+    mock_info_provider
+        .expect_token_info()
+        .times(6)
+        .returning(move |_| bail!("No token info"));
     mock_info_provider
         .expect_full_address_info_search()
         .times(12)
@@ -371,7 +380,10 @@ async fn service_txs_to_tx_list_items_last_timestamp_defined_and_same() {
 
     let mut mock_info_provider = MockInfoProvider::new();
     mock_info_provider.expect_safe_info().times(0);
-    mock_info_provider.expect_token_info().times(0);
+    mock_info_provider
+        .expect_token_info()
+        .times(6)
+        .returning(move |_| bail!("No token info"));
     mock_info_provider
         .expect_full_address_info_search()
         .times(12)
@@ -422,7 +434,10 @@ async fn service_txs_to_tx_list_items_last_timestamp_defined_and_same() {
 async fn service_txs_to_tx_list_items_date_label_berlin_timezone() {
     let mut mock_info_provider = MockInfoProvider::new();
     mock_info_provider.expect_safe_info().times(0);
-    mock_info_provider.expect_token_info().times(0);
+    mock_info_provider
+        .expect_token_info()
+        .times(6)
+        .returning(move |_| bail!("No token info"));
     mock_info_provider
         .expect_full_address_info_search()
         .times(12)
@@ -476,7 +491,10 @@ async fn service_txs_to_tx_list_items_date_label_berlin_timezone() {
 async fn service_txs_to_tx_list_items_date_label_melbourne_timezone() {
     let mut mock_info_provider = MockInfoProvider::new();
     mock_info_provider.expect_safe_info().times(0);
-    mock_info_provider.expect_token_info().times(0);
+    mock_info_provider
+        .expect_token_info()
+        .times(6)
+        .returning(move |_| bail!("No token info"));
     mock_info_provider
         .expect_full_address_info_search()
         .times(12)
@@ -534,7 +552,10 @@ async fn service_txs_to_tx_list_items_date_label_melbourne_timezone() {
 async fn service_txs_to_tx_list_items_date_label_buenos_aires_timezone() {
     let mut mock_info_provider = MockInfoProvider::new();
     mock_info_provider.expect_safe_info().times(0);
-    mock_info_provider.expect_token_info().times(0);
+    mock_info_provider
+        .expect_token_info()
+        .times(6)
+        .returning(move |_| bail!("No token info"));
     mock_info_provider
         .expect_full_address_info_search()
         .times(12)
@@ -612,7 +633,10 @@ async fn peek_timestamp_and_remove_item_with_items() {
 
     let mut mock_info_provider = MockInfoProvider::new();
     mock_info_provider.expect_safe_info().times(0);
-    mock_info_provider.expect_token_info().times(0);
+    mock_info_provider
+        .expect_token_info()
+        .times(1)
+        .returning(move |_| bail!("No token info"));
     mock_info_provider
         .expect_full_address_info_search()
         .times(1)

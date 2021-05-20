38a3bd60656ee89c8aa3406d36c80f8c6f0eaf64
Jose Alvarez <jp.alvarezl@gmail.com>
2021-05-20 16:10:34 +0200

    Added tests for safeTxHash and domainHash calculation with v1.3.0 safe

--- src/utils/tests/transactions.rs
+++ src/utils/tests/transactions.rs
@@ -1,3 +1,4 @@
+use crate::providers::info::SafeInfo;
 use crate::utils::transactions::{cancellation_parts_hash, domain_hash, hash};
 use ethcontract_common::hash::keccak256;
 use ethereum_types::Address;
@@ -8,9 +9,10 @@ fn domain_hash_for_safe_address() {
         "0xd6f5Bef6bb4acD235CF85c0ce196316d10785d67".to_string(),
     ))
     .unwrap();
-    let actual = to_hex_string!(domain_hash(&safe_address, false).to_vec());
+    let actual =
+        to_hex_string!(domain_hash(&safe_address, build_safe_info("1.3.0".to_string())).to_vec());
     assert_eq!(
-        "0x6dda5da6f3b6225311946ab4732b5658018db6dc890378fbdb529d8e9832762a",
+        "0x0d56532a2a780ffd32b2c3d85d0f8a7b2fc13df0576c006e2aaa47eb66cf71c9",
         actual
     );
 }
@@ -21,7 +23,8 @@ fn domain_hash_for_safe_address_legacy() {
         "0xd6f5Bef6bb4acD235CF85c0ce196316d10785d67".to_string(),
     ))
     .unwrap();
-    let actual = to_hex_string!(domain_hash(&safe_address, true).to_vec());
+    let actual =
+        to_hex_string!(domain_hash(&safe_address, build_safe_info("1.1.1".to_string())).to_vec());
     assert_eq!(
         "0x6dda5da6f3b6225311946ab4732b5658018db6dc890378fbdb529d8e9832762a",
         actual
@@ -29,17 +32,33 @@ fn domain_hash_for_safe_address_legacy() {
 }
 
 #[test]
-fn safe_tx_hash_for_safe_address_cancellation_tx() {
+fn safe_tx_hash_for_safe_address_cancellation_tx_legacy() {
     let safe_address: Address = serde_json::from_value(serde_json::value::Value::String(
         "0xd6f5Bef6bb4acD235CF85c0ce196316d10785d67".to_string(),
     ))
     .unwrap();
     let nonce = 39;
 
-    let domain_hash = domain_hash(&safe_address, true);
+    let domain_hash = domain_hash(&safe_address, build_safe_info("1.1.1".to_string()));
     let actual = to_hex_string!(hash(safe_address, nonce, domain_hash).to_vec());
     assert_eq!(
         "0x89067bfebe450e45c02dd97e3cc9bd1656d49ebb8a17819829eab9c5dc575c27",
+        actual
+    );
+}
+
+#[test]
+fn safe_tx_hash_for_safe_address_cancellation_tx() {
+    let safe_address: Address = serde_json::from_value(serde_json::value::Value::String(
+        "0x4cb09344de5bCCD45F045c5Defa0E0452869FF0f".to_string(),
+    ))
+    .unwrap();
+    let nonce = 39;
+
+    let domain_hash = domain_hash(&safe_address, build_safe_info("1.3.0".to_string()));
+    let actual = to_hex_string!(hash(safe_address, nonce, domain_hash).to_vec());
+    assert_eq!(
+        "0xdce3bf453ed8cf84d13c76911e5d11c31501b24004b9e856d6091808067bd398",
         actual
     );
 }
@@ -65,4 +84,17 @@ fn empty_data_keccak() {
         to_hex_string!(keccak256(vec![])),
         "0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470"
     );
+}
+
+fn build_safe_info(version: String) -> Option<SafeInfo> {
+    Some(SafeInfo {
+        address: "".to_string(),
+        nonce: 0,
+        threshold: 0,
+        owners: vec![],
+        master_copy: "".to_string(),
+        modules: None,
+        fallback_handler: None,
+        version: Some(version),
+    })
 }
--- src/utils/transactions.rs
+++ src/utils/transactions.rs
@@ -1,15 +1,17 @@
 use crate::cache::cache_operations::RequestCached;
 use crate::config::{base_transaction_service_url, chain_id, transaction_request_timeout};
 use crate::models::backend::transactions::MultisigTransaction;
-use crate::providers::info::{DefaultInfoProvider, InfoProvider, SAFE_V_1_3_0};
+use crate::providers::info::{DefaultInfoProvider, InfoProvider, SafeInfo, SAFE_V_1_3_0};
 use crate::utils::context::Context;
 use ethabi::Uint;
 use ethcontract_common::hash::keccak256;
 use ethereum_types::{Address, H256};
 use semver::Version;
 
-pub const DOMAIN_SEPARATOR_TYPEHASH: &'static str =
+pub const DOMAIN_SEPARATOR_TYPEHASH_LEGACY: &'static str =
     "0x035aff83d86937d35b32e04f0ddc6ff469290eef2f1b692d8a815c89404d4749";
+pub const DOMAIN_SEPARATOR_TYPEHASH: &'static str =
+    "0x47e79534a245952e8b16893a336b85a3d9ea9fa8c573f3d803afb92a79469218";
 pub const SAFE_TX_TYPEHASH: &'static str =
     "0xbb8310d486368db6bd6f849402fdd73ad53d316b5a4b2644ad6efe0f941286d8";
 
@@ -24,22 +26,11 @@ pub async fn fetch_rejections(
     let info_provider = DefaultInfoProvider::new(&context);
     let safe_info = info_provider.safe_info(safe_address).await.ok();
 
-    let version = safe_info
-        .as_ref()
-        .and_then(|safe_info| safe_info.version.as_ref().map(|it| Version::parse(it).ok()))
-        .flatten();
-
     let safe_address: Address =
         serde_json::from_value(serde_json::value::Value::String(safe_address.to_string())).unwrap();
 
-    let is_legacy = if let Some(version) = version {
-        version <= Version::parse(SAFE_V_1_3_0).unwrap_or(Version::new(1, 3, 0))
-    } else {
-        true
-    };
-
     let safe_tx_hash =
-        to_hex_string!(hash(safe_address, nonce, domain_hash(&safe_address, is_legacy)).to_vec());
+        to_hex_string!(hash(safe_address, nonce, domain_hash(&safe_address, safe_info)).to_vec());
 
     let multisig_tx = fetch_cancellation_tx(context, safe_tx_hash).await;
     multisig_tx
@@ -69,9 +60,16 @@ pub(super) fn hash(safe_address: Address, nonce: u64, domain_hash: [u8; 32]) -> 
     keccak256(encoded)
 }
 
-pub(super) fn domain_hash(safe_address: &Address, is_legacy: bool) -> [u8; 32] {
+pub(super) fn domain_hash(safe_address: &Address, safe_info: Option<SafeInfo>) -> [u8; 32] {
+    let is_legacy = is_legacy_domain_separator(safe_info);
+    let domain_separator_typehash = if is_legacy {
+        DOMAIN_SEPARATOR_TYPEHASH_LEGACY
+    } else {
+        DOMAIN_SEPARATOR_TYPEHASH
+    };
+
     let domain_separator: H256 =
-        serde_json::from_value(serde_json::Value::String(DOMAIN_SEPARATOR_TYPEHASH.into()))
+        serde_json::from_value(serde_json::Value::String(domain_separator_typehash.into()))
             .unwrap();
 
     let encoded = if is_legacy {
@@ -109,6 +107,19 @@ pub(super) fn cancellation_parts_hash(safe_address: &Address, nonce: u64) -> [u8
     ]);
 
     keccak256(encoded_parts)
+}
+
+pub(super) fn is_legacy_domain_separator(safe_info: Option<SafeInfo>) -> bool {
+    let version = safe_info
+        .as_ref()
+        .and_then(|safe_info| safe_info.version.as_ref().map(|it| Version::parse(it).ok()))
+        .flatten();
+
+    if let Some(version) = version {
+        version < Version::parse(SAFE_V_1_3_0).unwrap_or(Version::new(1, 3, 0))
+    } else {
+        true
+    }
 }
 
 // We silently fail if the cancellation transaction is not found

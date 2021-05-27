85dbcb9ae1b731cb3dad7af5af76664524a3bcfe
Jose Alvarez <jp.alvarezl@gmail.com>
2021-05-27 12:01:20 +0200

    Passed only the version on to function

--- src/utils/transactions.rs
+++ src/utils/transactions.rs
@@ -2,7 +2,7 @@ use crate::cache::cache_operations::RequestCached;
 use crate::config::{base_transaction_service_url, chain_id, transaction_request_timeout};
 use crate::models::backend::transactions::MultisigTransaction;
 use crate::providers::info::SAFE_V_1_3_0;
-use crate::providers::info::{DefaultInfoProvider, InfoProvider, SafeInfo};
+use crate::providers::info::{DefaultInfoProvider, InfoProvider};
 use crate::utils::context::Context;
 use ethabi::Uint;
 use ethcontract_common::hash::keccak256;
@@ -26,7 +26,11 @@ pub async fn fetch_rejections(
 ) -> Option<Vec<String>> {
     let info_provider = DefaultInfoProvider::new(&context);
     let safe_info = info_provider.safe_info(safe_address).await.ok();
-    let is_legacy = use_legacy_domain_separator(safe_info);
+    let version = safe_info
+        .as_ref()
+        .and_then(|safe_info| safe_info.version.as_ref().map(|it| Version::parse(it).ok()))
+        .flatten();
+    let is_legacy = use_legacy_domain_separator(version);
     let safe_address: Address =
         serde_json::from_value(serde_json::value::Value::String(safe_address.to_string())).unwrap();
     let domain_hash = if is_legacy {
@@ -82,7 +86,7 @@ pub(super) fn domain_hash_v100(safe_address: &Address) -> [u8; 32] {
     let domain_separator: H256 = serde_json::from_value(serde_json::Value::String(
         DOMAIN_SEPARATOR_TYPEHASH_LEGACY.into(),
     ))
-    .unwrap();
+        .unwrap();
 
     let encoded = ethabi::encode(&[
         ethabi::Token::Uint(Uint::from(domain_separator.0)),
@@ -112,12 +116,7 @@ pub(super) fn cancellation_parts_hash(safe_address: &Address, nonce: u64) -> [u8
     keccak256(encoded_parts)
 }
 
-pub(super) fn use_legacy_domain_separator(safe_info: Option<SafeInfo>) -> bool {
-    let version = safe_info
-        .as_ref()
-        .and_then(|safe_info| safe_info.version.as_ref().map(|it| Version::parse(it).ok()))
-        .flatten();
-
+pub(super) fn use_legacy_domain_separator(version: Option<Version>) -> bool {
     if let Some(version) = version.as_ref() {
         version < &SAFE_V_1_3_0
     } else {

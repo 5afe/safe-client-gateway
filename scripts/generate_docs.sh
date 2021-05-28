a498b65875cadc79e5646a64098dc522f2795e23
Jose Alvarez <jp.alvarezl@gmail.com>
2021-05-28 15:45:29 +0200

    Made cargo doc call multiline

--- scripts/generate_docs.sh
+++ scripts/generate_docs.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 rm -rf ../target/doc
-cargo doc --no-deps --exclude '*json*' --workspace
+
+cargo doc --no-deps \
+  --exclude '*json*' \
+  --workspace
+  
 open ../target/doc/safe_client_gateway/index.html # eventually this would be a deployment step

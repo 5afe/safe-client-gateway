fb35611d5733822a903e1aa341b04360408689b6
Jose Alvarez <jp.alvarezl@gmail.com>
2021-06-17 11:10:43 +0200

    Added response sample json for data decoder

--- src/routes/utils.rs
+++ src/routes/utils.rs
@@ -31,6 +31,25 @@ use rocket::serde::json::Json;
  *   "data": "0x095ea7b3000000000000000000000000ae9844f89d98c150f5e61bfc676d68b4921559900000000000000000000000000000000000000000000000000001c6bf52634000"
  * }
  * ```
+ * This results in:
+ *
+ * ```json
+ * {
+ *   "method": "approve",
+ *   "parameters": [
+ *    {
+ *       "name": "spender",
+ *       "type": "address",
+ *       "value": "0xae9844F89D98c150F5e61bfC676D68b492155990"
+ *     },
+ *     {
+ *       "name": "value",
+ *       "type": "uint256",
+ *       "value": "500000000000000"
+ *     }
+ *   ]
+ * }
+ * ```
  *
  */
 #[post(

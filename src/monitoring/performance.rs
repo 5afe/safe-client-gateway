b5e3b8c52e702868340d9b5dcbf5ce0042b7a8a8
Jose Alvarez <jp.alvarezl@gmail.com>
2021-07-12 10:45:34 +0200

    WIP

--- src/monitoring/performance.rs
+++ src/monitoring/performance.rs
@@ -1,6 +1,11 @@
 use chrono::Utc;
+use lazy_static::lazy_static;
 use rocket::fairing::{Fairing, Info, Kind};
 use rocket::{Data, Request, Response};
+
+lazy_static! {
+    static ref CHAIN_ID_PATH_PATTERN: Regex = Regex::new(r"/v1/chains/d{+}/+").unwrap();
+}
 
 pub struct PerformanceMonitor();
 
@@ -18,6 +23,19 @@ impl Fairing for PerformanceMonitor {
     }
 
     async fn on_response<'r>(&self, request: &'r Request<'_>, response: &mut Response<'r>) {
+        let raw_path_string = request.uri().path().to_string();
+        let raw_path = request.uri().path();
+
+        log::error!(
+            "raw path: {:#?}",
+            &raw_path.segments().find(|segment| segment == &"chains")
+        );
+        let chain_id = if raw_path_string.contains("/v1/chains/*/*") {
+            raw_path_string.find("/v1/chains//")
+        } else {
+            None
+        };
+
         let path_data = request
             .route()
             .map(|route| route.uri.to_string())
@@ -28,6 +46,18 @@ impl Fairing for PerformanceMonitor {
         let method = request.method().as_str();
         let status_code = response.status().code;
         let delta = Utc::now().timestamp_millis() - cached;
-        log::info!("MT::{}::{}::{}::{}", method, path_data, delta, status_code)
+        if chain_id.is_some() {
+            log::info!(
+                "MT::{}::{}::{}::{}::{}::{}",
+                method,
+                raw_path_string,
+                delta,
+                status_code,
+                path_data,
+                chain_id.unwrap()
+            )
+        } else {
+            log::info!("MT::{}::{}::{}::{}", method, path_data, delta, status_code)
+        }
     }
 }

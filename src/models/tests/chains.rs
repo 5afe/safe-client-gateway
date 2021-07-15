7a71e40213a795234d53ea9b6db17789104bdece
Jose Alvarez <jp.alvarezl@gmail.com>
2021-07-15 11:59:54 +0200

    Removed inlined json

--- src/models/tests/chains.rs
+++ src/models/tests/chains.rs
@@ -2,30 +2,9 @@ use crate::models::backend::chains::{ChainInfo, NativeCurrency, Theme};
 use crate::models::service::chains::{
     ChainInfo as ServiceChainInfo, NativeCurrency as ServiceNativeCurrency, Theme as ServiceTheme,
 };
-use rocket::serde::json::json;
 
 #[test]
 fn chain_info_json() {
-    let chain_info_json = json!({
-          "chainId": "4",
-          "chainName": "Rinkeby",
-          "rpcUrl": "https://someurl.com/rpc",
-          "blockExplorerUrl": "https://blockexplorer.com",
-          "transactionService": "https://safe-transaction.rinkeby.staging.gnosisdev.com",
-          "nativeCurrency": {
-            "name": "Ether",
-            "symbol": "ETH",
-            "decimals": 18,
-            "logoUrl": "https://test.token.image.url",
-          },
-          "theme": {
-            "textColor": "#fff",
-            "backgroundColor": "#000"
-          },
-          "ensRegistryAddress": "0xFFfFfFffFFfffFFfFFfFFFFFffFFFffffFfFFFfF",
-          "recommendedMasterCopyVersion": "1.1.1"
-    });
-
     let expected = ChainInfo {
         recommended_master_copy_version: "1.1.1".to_string(),
         transaction_service: "https://safe-transaction.rinkeby.staging.gnosisdev.com".to_string(),
@@ -46,7 +25,7 @@ fn chain_info_json() {
         ens_registry_address: Some("0xFFfFfFffFFfffFFfFFfFFFFFffFFFffffFfFFFfF".to_string()),
     };
 
-    let actual = serde_json::from_str::<ChainInfo>(&chain_info_json.to_string());
+    let actual = serde_json::from_str::<ChainInfo>(crate::json::CHAIN_INFO_RINKEBY);
 
     assert!(actual.is_ok());
     assert_eq!(expected, actual.unwrap());
@@ -54,26 +33,6 @@ fn chain_info_json() {
 
 #[test]
 fn chain_info_json_to_service_chain_info() {
-    let chain_info_json = json!({
-          "chainId": "4",
-          "chainName": "Rinkeby",
-          "rpcUrl": "https://someurl.com/rpc",
-          "blockExplorerUrl": "https://blockexplorer.com",
-          "transactionService": "https://safe-transaction.rinkeby.staging.gnosisdev.com",
-          "nativeCurrency": {
-            "name": "Ether",
-            "symbol": "ETH",
-            "decimals": 18,
-            "logoUrl": "https://test.token.image.url",
-          },
-          "theme": {
-            "textColor": "#fff",
-            "backgroundColor": "#000"
-          },
-          "ensRegistryAddress": "0xFFfFfFffFFfffFFfFFfFFFFFffFFFffffFfFFFfF",
-          "recommendedMasterCopyVersion": "1.1.1"
-    });
-
     let expected = ServiceChainInfo {
         transaction_service: "https://safe-transaction.rinkeby.staging.gnosisdev.com".to_string(),
         chain_id: "4".to_string(),
@@ -93,7 +52,7 @@ fn chain_info_json_to_service_chain_info() {
         ens_registry_address: Some("0xFFfFfFffFFfffFFfFFfFFFFFffFFFffffFfFFFfF".to_string()),
     };
 
-    let from_json = serde_json::from_str::<ChainInfo>(&chain_info_json.to_string()).unwrap();
+    let from_json = serde_json::from_str::<ChainInfo>(crate::json::CHAIN_INFO_RINKEBY).unwrap();
     let actual: ServiceChainInfo = from_json.into();
 
     assert_eq!(expected, actual);

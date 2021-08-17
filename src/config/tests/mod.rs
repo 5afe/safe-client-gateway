6a1be633f95f902605a4364ca541125904778a87
Jose Alvarez <jp.alvarezl@gmail.com>
2021-08-17 16:21:20 +0200

    Added structs and impls for u64 env vars

--- src/config/tests/mod.rs
+++ src/config/tests/mod.rs
@@ -1,12 +1,17 @@
 #[test]
 fn test_env() {
-    for test_case in build_test_cases() {
+    for test_case in build_usize_test_cases() {
+        test_case.assert_default();
+        test_case.assert_env_key();
+    }
+
+    for test_case in build_u64_test_cases() {
         test_case.assert_default();
         test_case.assert_env_key();
     }
 }
 
-fn build_test_cases() -> Vec<impl TestCase> {
+fn build_usize_test_cases() -> Vec<USizeEnvValue> {
     vec![
         USizeEnvValue {
             expected_default: 60 * 60,
@@ -56,6 +61,21 @@ fn build_test_cases() -> Vec<impl TestCase> {
     ]
 }
 
+fn build_u64_test_cases() -> Vec<U64EnvValue> {
+    vec![
+        U64EnvValue {
+            expected_default: 1000,
+            env_key: String::from("INTERNAL_CLIENT_CONNECT_TIMEOUT"),
+            generator: Box::new(super::internal_client_connect_timeout),
+        },
+        U64EnvValue {
+            expected_default: 3000,
+            env_key: String::from("SAFE_APP_INFO_REQUEST_TIMEOUT"),
+            generator: Box::new(super::safe_app_info_request_timeout),
+        },
+    ]
+}
+
 trait TestCase {
     fn assert_default(&self);
     fn assert_env_key(&self);
@@ -68,6 +88,35 @@ struct USizeEnvValue {
 }
 
 impl TestCase for USizeEnvValue {
+    fn assert_default(&self) {
+        std::env::remove_var(&self.env_key);
+        let actual_default = (&self.generator)();
+        assert_eq!(
+            self.expected_default, actual_default,
+            "Test default value for env key: {}",
+            &self.env_key
+        );
+    }
+
+    fn assert_env_key(&self) {
+        let mock_env_var_value = 1;
+        std::env::set_var(&self.env_key, &mock_env_var_value.to_string());
+        let actual_env = (&self.generator)();
+        assert_eq!(
+            mock_env_var_value, actual_env,
+            "Test env var for env key: {}",
+            &self.env_key
+        );
+    }
+}
+
+struct U64EnvValue {
+    expected_default: u64,
+    env_key: String,
+    generator: Box<dyn Fn() -> u64>,
+}
+
+impl TestCase for U64EnvValue {
     fn assert_default(&self) {
         std::env::remove_var(&self.env_key);
         let actual_default = (&self.generator)();

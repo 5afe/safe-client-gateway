f7c02f924367addfe0468c1c26b0a50391d6ec2b
Jose Alvarez <jp.alvarezl@gmail.com>
2021-09-07 15:06:53 +0200

    Added models for safe_apps

---
+++ src/models/backend/safe_app.rs
@@ -0,0 +1,20 @@
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug, PartialEq, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct SafeApp {
+    pub id: u64,
+    pub url: String,
+    pub name: String,
+    pub icon_url: String,
+    pub description: String,
+    pub chain_ids: Vec<u64>,
+    pub provider: Vec<SafeAppProvider>,
+}
+
+#[derive(Deserialize, Debug, PartialEq, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct SafeAppProvider {
+    pub url: String,
+    pub name: String,
+}

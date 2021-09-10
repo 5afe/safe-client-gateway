dbb0cd28a82fbf992a2fe74a68a57000a1e81f03
Daniel Agustin Tradito <datradito@gmail.com>
2021-09-10 09:50:07 -0300

    feat: adding moonbase config

--- src/json/mod.rs
+++ src/json/mod.rs
@@ -121,6 +121,7 @@ pub const BALANCE_COMPOUND_ETHER: &str = include_str!("balances/balance_compound
 pub const TX_DETAILS_WITH_ORIGIN: &str = include_str!("results/tx_details_with_origin.json");
 
 pub const CHAIN_INFO_RINKEBY: &str = include_str!("chains/rinkeby.json");
+pub const CHAIN_INFO_MOONBASE: &str = include_str!("chains/moonbase.json");
 pub const CHAIN_INFO_RINKEBY_FIXED_GAS_PRICE: &str = include_str!("chains/rinkeby_fixed_gas_price.json");
 pub const CHAIN_INFO_RINKEBY_MULTIPLE_GAS_PRICE: &str = include_str!("chains/rinkeby_multiple_gas_price.json");
 pub const CHAIN_INFO_RINKEBY_NO_GAS_PRICE: &str = include_str!("chains/rinkeby_no_gas_price.json");

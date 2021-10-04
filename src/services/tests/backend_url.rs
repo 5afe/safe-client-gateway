c9d38b810c591b2097b5e4007945af4a5e09dc3c
Jose Alvarez <jp.alvarezl@gmail.com>
2021-10-04 08:41:07 +0200

    Added tests for debug build asserting different paths

--- src/services/tests/backend_url.rs
+++ src/services/tests/backend_url.rs
@@ -5,6 +5,7 @@ use crate::providers::info::*;
 use crate::utils::errors::ApiResult;
 
 #[rocket::async_test]
+#[cfg(not(debug_assertions))]
 async fn core_uri_success_with_params() {
     let safe_address = "0x1230B3d59858296A31053C1b8562Ecf89A2f888b";
     let trusted = false;
@@ -58,6 +59,7 @@ async fn core_uri_success_with_params() {
 }
 
 #[rocket::async_test]
+#[cfg(not(debug_assertions))]
 async fn core_uri_success_without_params() {
     let chain_info = ChainInfo {
         recommended_master_copy_version: "1.1.1".to_string(),
@@ -100,6 +102,108 @@ async fn core_uri_success_without_params() {
 
     assert_eq!(
         "http://mainnet-safe-transaction-web.safe.svc.cluster.local/api/some/path",
+        url.unwrap()
+    );
+}
+
+#[rocket::async_test]
+#[cfg(debug_assertions)]
+async fn core_uri_success_with_params() {
+    let safe_address = "0x1230B3d59858296A31053C1b8562Ecf89A2f888b";
+    let trusted = false;
+    let exclude_spam = true;
+    let chain_info = ChainInfo {
+        recommended_master_copy_version: "1.1.1".to_string(),
+        transaction_service: "https://safe-transaction.mainnet.gnosis.io".to_string(),
+        vpc_transaction_service: "http://mainnet-safe-transaction-web.safe.svc.cluster.local"
+            .to_string(),
+        chain_id: "1".to_string(),
+        chain_name: "".to_string(),
+        l2: false,
+        description: "Random description".to_string(),
+        rpc_uri: RpcUri {
+            authentication: RpcAuthentication::ApiKeyPath,
+            value: "".to_string(),
+        },
+        block_explorer_uri_template: BlockExplorerUriTemplate {
+            address: "".to_string(),
+            tx_hash: "".to_string(),
+        },
+        native_currency: NativeCurrency {
+            name: "".to_string(),
+            symbol: "".to_string(),
+            decimals: 0,
+            logo_uri: "https://test.token.image.url".to_string(),
+        },
+        theme: Theme {
+            text_color: "#fff".to_string(),
+            background_color: "#000".to_string(),
+        },
+        ens_registry_address: None,
+        gas_price: vec![GasPrice::Fixed {
+            wei_value: "1000000".to_string(),
+        }],
+    };
+    let mut mock_info_provider = MockInfoProvider::new();
+    mock_info_provider
+        .expect_chain_info()
+        .times(1)
+        .return_once(move || Ok(chain_info));
+    let url = core_uri!(
+        mock_info_provider,
+        "/v1/safes/{}/balances/usd/?trusted={}&exclude_spam={}",
+        safe_address,
+        trusted,
+        exclude_spam
+    );
+
+    assert_eq!(url.unwrap(), "https://safe-transaction.mainnet.gnosis.io/api/v1/safes/0x1230B3d59858296A31053C1b8562Ecf89A2f888b/balances/usd/?trusted=false&exclude_spam=true".to_string());
+}
+
+#[rocket::async_test]
+#[cfg(debug_assertions)]
+async fn core_uri_success_without_params() {
+    let chain_info = ChainInfo {
+        recommended_master_copy_version: "1.1.1".to_string(),
+        transaction_service: "https://safe-transaction.mainnet.gnosis.io".to_string(),
+        vpc_transaction_service: "http://mainnet-safe-transaction-web.safe.svc.cluster.local"
+            .to_string(),
+        chain_id: "1".to_string(),
+        chain_name: "".to_string(),
+        l2: false,
+        description: "Random description".to_string(),
+        rpc_uri: RpcUri {
+            authentication: RpcAuthentication::ApiKeyPath,
+            value: "".to_string(),
+        },
+        block_explorer_uri_template: BlockExplorerUriTemplate {
+            address: "".to_string(),
+            tx_hash: "".to_string(),
+        },
+        native_currency: NativeCurrency {
+            name: "".to_string(),
+            symbol: "".to_string(),
+            decimals: 0,
+            logo_uri: "https://test.token.image.url".to_string(),
+        },
+        theme: Theme {
+            text_color: "#fff".to_string(),
+            background_color: "#000".to_string(),
+        },
+        ens_registry_address: None,
+        gas_price: vec![GasPrice::Fixed {
+            wei_value: "1000000".to_string(),
+        }],
+    };
+    let mut mock_info_provider = MockInfoProvider::new();
+    mock_info_provider
+        .expect_chain_info()
+        .times(1)
+        .return_once(move || Ok(chain_info));
+    let url = core_uri!(mock_info_provider, "/some/path");
+
+    assert_eq!(
+        "https://safe-transaction.mainnet.gnosis.io/api/some/path",
         url.unwrap()
     );
 }

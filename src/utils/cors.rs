cefbee6ef883d418b75267956a3f51a8bd7642fc
Jose Alvarez <jpalvarezl@users.noreply.github.com>
2021-10-05 17:58:24 +0200

    204 status code forced for a pre-flight OPTION requests (#640)
    * 204 status code forced for a pre-flight OPTION requests
    
    * Retrigger workflow
    
    * Removed TODO

--- src/utils/cors.rs
+++ src/utils/cors.rs
@@ -1,7 +1,6 @@
 use rocket::fairing::{Fairing, Info, Kind};
-use rocket::http::{ContentType, Header, Method};
+use rocket::http::{ContentType, Header, Method, Status};
 use rocket::{Request, Response};
-use std::io::Cursor;
 
 pub struct CORS();
 
@@ -15,7 +14,7 @@ impl Fairing for CORS {
     }
 
     async fn on_response<'r>(&self, request: &'r Request<'_>, response: &mut Response<'r>) {
-        // TODO https://github.com/lawliet89/rocket_cors/blob/master/examples/fairing.rs
+        //https://github.com/lawliet89/rocket_cors/blob/master/examples/fairing.rs
         if request.method() == Method::Options || response.content_type() == Some(ContentType::JSON)
         {
             response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
@@ -29,10 +28,10 @@ impl Fairing for CORS {
             ));
             response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
         }
-
-        if request.method() == Method::Options {
+        if request.method() == Method::Options && request.route().is_none() {
             response.set_header(ContentType::Plain);
-            response.set_sized_body(0, Cursor::new(""));
+            response.set_status(Status::NoContent);
+            let _ = response.body_mut().take();
         }
     }
 }

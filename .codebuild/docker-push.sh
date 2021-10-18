ff94ea739d6bf10dd50ef8ec78e85bd8a705cf79
Veaceslav Doina <20563034+veaceslavdoina@users.noreply.github.com>
2021-10-18 15:45:04 +0300

    Fixed push issue

--- .codebuild/docker-push.sh
+++ .codebuild/docker-push.sh
@@ -4,11 +4,12 @@
 repository=$1
 
 # Push Docker images to the repository
+now=`date +%Y-%m-%d--%H-%M-%S`
 for image in gateway_nginx gateway_web
 do
   docker tag $image:latest $repository-$image:latest
   docker push $repository-$image:latest
-  docker tag $image:latest $repository-$image:`date +%Y-%m-%d--%H-%M-%S`
-  docker push $repository-$image:`date +%Y-%m-%d--%H-%M-%S`
+  docker tag $image:latest $repository-$image:$now
+  docker push $repository-$image:$now
 done
 

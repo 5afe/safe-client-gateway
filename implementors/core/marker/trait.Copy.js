a2d1171eef9367f0595640f7b1b6d6b528626f1c
fmrsabino <fmrsabino@users.noreply.github.com>
2021-10-25 13:04:41 +0000

    deploy: 20c266c83a73b75d91bb7a00ca197362da6e4dfb

--- implementors/core/marker/trait.Copy.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {};
-implementors["safe_client_gateway"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.55.0/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"safe_client_gateway/models/commons/enum.Operation.html\" title=\"enum safe_client_gateway::models::commons::Operation\">Operation</a>","synthetic":false,"types":["safe_client_gateway::models::commons::Operation"]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()

519e87bf08aa8235ea0a24e4d1a72b15bea6b5b6
Jose Alvarez <jp.alvarezl@gmail.com>
2021-11-23 17:56:47 +0100

    Added remaining tests

--- src/providers/tests/fiat.rs
+++ src/providers/tests/fiat.rs
@@ -4,13 +4,15 @@ use crate::{
     providers::fiat::{Exchange, FiatInfoProvider},
     utils::{
         context::RequestContext,
-        errors::{ApiError, ErrorDetails},
+        errors::ApiError,
         http_client::{HttpClient, MockHttpClient, Request, Response},
     },
 };
+use bigdecimal::BigDecimal;
 use mockall::predicate::eq;
 use serde_json::json;
-use std::{sync::Arc, time::Duration};
+use std::str::FromStr;
+use std::sync::Arc;
 
 const EXCHANGE_API_BASE_URI: &'static str = "https://test.exchange-rate.api";
 const EXCHANGE_API_KEY: &'static str = "some_random_key";
@@ -96,6 +98,144 @@ async fn available_currency_codes_api_error() {
     let fiat_provider = FiatInfoProvider::new(&context);
 
     let actual = fiat_provider.available_currency_codes().await;
+
+    let expected = Err(ApiError::new_from_message_with_code(
+        500,
+        String::from("Unknown 'Exchange' json structure"),
+    ));
+    assert_eq!(expected, actual);
+}
+
+#[rocket::async_test]
+async fn exchange_usd_to() {
+    setup_exchange_env();
+    let cache = Arc::new(create_service_cache()) as Arc<dyn Cache>;
+    cache.invalidate_pattern("*");
+
+    let mut mock_http_client = MockHttpClient::new();
+    let request = Request::new(format!(
+        "{}?access_key={}",
+        EXCHANGE_API_BASE_URI, EXCHANGE_API_KEY
+    ));
+
+    mock_http_client
+        .expect_get()
+        .times(1)
+        .with(eq(request))
+        .returning(move |_| {
+            Ok(Response {
+                status_code: 200,
+                body: String::from(crate::tests::json::EXCHANGE_CURRENCY_RATES),
+            })
+        });
+    let context = RequestContext::setup_for_test(
+        String::from("request_id"),
+        String::from("host"),
+        &(Arc::new(mock_http_client) as Arc<dyn HttpClient>),
+        &cache,
+    );
+    let fiat_provider = FiatInfoProvider::new(&context);
+    let expected = Ok(1 / BigDecimal::from_str("1.125036").unwrap());
+
+    let actual = fiat_provider.exchange_usd_to("EUR").await;
+
+    assert_eq!(expected, actual);
+}
+
+#[rocket::async_test]
+async fn exchange_usd_to_usd() {
+    setup_exchange_env();
+    let cache = Arc::new(create_service_cache()) as Arc<dyn Cache>;
+    cache.invalidate_pattern("*");
+
+    let mut mock_http_client = MockHttpClient::new();
+    mock_http_client.expect_get().times(0);
+    let context = RequestContext::setup_for_test(
+        String::from("request_id"),
+        String::from("host"),
+        &(Arc::new(mock_http_client) as Arc<dyn HttpClient>),
+        &cache,
+    );
+    let fiat_provider = FiatInfoProvider::new(&context);
+    let expected = Ok(BigDecimal::from(1));
+
+    let actual = fiat_provider.exchange_usd_to("USD").await;
+
+    assert_eq!(expected, actual);
+}
+
+#[rocket::async_test]
+async fn exchange_usd_to_unknown_code() {
+    setup_exchange_env();
+    let cache = Arc::new(create_service_cache()) as Arc<dyn Cache>;
+    cache.invalidate_pattern("*");
+
+    let mut mock_http_client = MockHttpClient::new();
+    let request = Request::new(format!(
+        "{}?access_key={}",
+        EXCHANGE_API_BASE_URI, EXCHANGE_API_KEY
+    ));
+
+    mock_http_client
+        .expect_get()
+        .times(1)
+        .with(eq(request))
+        .returning(move |_| {
+            Ok(Response {
+                status_code: 200,
+                body: String::from(crate::tests::json::EXCHANGE_CURRENCY_RATES),
+            })
+        });
+    let context = RequestContext::setup_for_test(
+        String::from("request_id"),
+        String::from("host"),
+        &(Arc::new(mock_http_client) as Arc<dyn HttpClient>),
+        &cache,
+    );
+    let fiat_provider = FiatInfoProvider::new(&context);
+    let expected = Err(ApiError::new_from_message_with_code(
+        422,
+        String::from("Currency not found"),
+    ));
+
+    let actual = fiat_provider.exchange_usd_to("UNKOWN_CURRENCY_CODE").await;
+
+    assert_eq!(expected, actual);
+}
+
+#[rocket::async_test]
+async fn exchange_usd_to_api_failure() {
+    setup_exchange_env();
+    let cache = Arc::new(create_service_cache()) as Arc<dyn Cache>;
+    cache.invalidate_pattern("*");
+    let api_error_json =
+        json!({"success":false,"error":{"code":105,"type":"base_currency_access_restricted"}});
+
+    let mut mock_http_client = MockHttpClient::new();
+    let request = Request::new(format!(
+        "{}?access_key={}",
+        EXCHANGE_API_BASE_URI, EXCHANGE_API_KEY
+    ));
+
+    mock_http_client
+        .expect_get()
+        .times(1)
+        .with(eq(request))
+        .returning(move |_| {
+            Ok(Response {
+                status_code: 200,
+                body: String::from(api_error_json.to_string()),
+            })
+        });
+    let context = RequestContext::setup_for_test(
+        String::from("request_id"),
+        String::from("host"),
+        &(Arc::new(mock_http_client) as Arc<dyn HttpClient>),
+        &cache,
+    );
+    let fiat_provider = FiatInfoProvider::new(&context);
+
+    let actual = fiat_provider.exchange_usd_to("EUR").await;
 
     let expected = Err(ApiError::new_from_message_with_code(
         500,

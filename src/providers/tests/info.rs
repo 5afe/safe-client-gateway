732e67e0266b6d8d0ca9003ce6a301a43e747263
Jose Alvarez <jp.alvarezl@gmail.com>
2021-11-25 16:41:27 +0100

    Added happy path for info provider test

--- src/providers/tests/info.rs
+++ src/providers/tests/info.rs
@@ -6,7 +6,10 @@ use crate::{
         chain_info_request_timeout, contract_info_request_timeout, safe_app_info_request_timeout,
         safe_info_request_timeout, token_info_request_timeout,
     },
-    providers::info::{DefaultInfoProvider, InfoProvider, SafeAppInfo, SafeInfo, TokenInfo},
+    providers::{
+        address_info::ContractInfo,
+        info::{DefaultInfoProvider, InfoProvider, SafeAppInfo, SafeInfo, TokenInfo},
+    },
     utils::{
         context::RequestContext,
         errors::{ApiError, ErrorDetails},
@@ -464,7 +467,59 @@ async fn default_info_provider_safe_app_info_not_found() {
 }
 
 #[rocket::async_test]
-async fn contract_info() {}
+async fn contract_info() {
+    let bip_contract_address = "0x00000000000045166C45aF0FC6E4Cf31D9E14B9A";
+    let cache = Arc::new(create_service_cache()) as Arc<dyn Cache>;
+
+    let mut mock_http_client = MockHttpClient::new();
+
+    let mut chain_request = Request::new(config_uri!("/v1/chains/{}/", 4));
+    chain_request.timeout(Duration::from_millis(chain_info_request_timeout()));
+    mock_http_client
+        .expect_get()
+        .times(1)
+        .with(eq(chain_request))
+        .returning(move |_| {
+            Ok(Response {
+                status_code: 200,
+                body: String::from(crate::tests::json::CHAIN_INFO_RINKEBY),
+            })
+        });
+
+    let mut contract_info_request = Request::new(format!(
+        "https://safe-transaction.rinkeby.staging.gnosisdev.com/api/v1/contracts/{}/",
+        &bip_contract_address
+    ));
+    contract_info_request.timeout(Duration::from_millis(contract_info_request_timeout()));
+
+    mock_http_client
+        .expect_get()
+        .times(1)
+        .with(eq(contract_info_request))
+        .returning(move |_| {
+            Ok(Response {
+                status_code: 202,
+                body: String::from(crate::tests::json::CONTRACT_INFO_BID),
+            })
+        });
+
+    let context = RequestContext::setup_for_test(
+        String::from(""),
+        config_uri!(""),
+        &(Arc::new(mock_http_client) as Arc<dyn HttpClient>),
+        &cache,
+    );
+    let expected =
+        serde_json::from_str::<ContractInfo>(crate::tests::json::CONTRACT_INFO_BID).unwrap();
+
+    let info_provider = DefaultInfoProvider::new("4", &context);
+    let actual = info_provider
+        .contract_info(bip_contract_address)
+        .await
+        .unwrap();
+
+    assert_eq!(expected, actual);
+}
 
 #[rocket::async_test]
 async fn contract_info_not_found() {

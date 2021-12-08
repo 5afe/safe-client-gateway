b323df39251a1d191feb4a1fa60f531e5f2e9f70
Jose Alvarez <jp.alvarezl@gmail.com>
2021-12-08 08:35:01 -0300

    Covered health endpoint

---
+++ src/routes/notifications/tests/routes.rs
@@ -0,0 +1,11 @@
+#[rocket::async_test]
+async fn delete_notification_success() {}
+
+#[rocket::async_test]
+async fn delete_notification_error() {}
+
+#[rocket::async_test]
+async fn post_notification_success() {}
+
+#[rocket::async_test]
+async fn post_notification_error() {}

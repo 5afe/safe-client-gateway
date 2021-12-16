180496590a595c9ebdf3ecbe71121ba5cf155200
Jose Alvarez <jp.alvarezl@gmail.com>
2021-12-16 10:43:35 -0300

    Removed duplication

--- src/routes/hooks/routes.rs
+++ src/routes/hooks/routes.rs
@@ -17,18 +17,15 @@ pub fn update(context: RequestContext, token: String, update: Json<Payload>) -> 
 #[post(
     "/v1/chains/<chain_id>/hook/update/<token>",
     format = "json",
-    data = "<update>"
+    data = "<payload>"
 )]
 pub fn post_hook_update(
     context: RequestContext,
     chain_id: String,
     token: String,
-    update: Json<Payload>,
+    payload: Json<Payload>,
 ) -> ApiResult<()> {
-    if token != webhook_token() {
-        bail!("Invalid token");
-    }
-    invalidate_caches(context.cache(), &update)
+    update(context, token, payload)
 }
 
 #[post("/v1/flush/<token>", format = "json", data = "<invalidation_pattern>")]

245a9378ef064ea263ec58cb316a3e22a472dc48
Jose Alvarez <jose.alvarez@gnosis.io>
2022-04-05 11:26:46 +0200

    Added QueryParam trait but WIP to abstract tx related requests

--- src/routes/transactions/handlers/transfers.rs
+++ src/routes/transactions/handlers/transfers.rs
@@ -1,6 +1,15 @@
 use crate::{
-    common::models::page::Page,
-    routes::transactions::models::{filters::TransferFilters, summary::TransactionListItem},
+    cache::cache_operations::RequestCached,
+    common::models::{
+        backend::transactions::Transaction,
+        page::{Page, PageMetadata},
+    },
+    config::transaction_request_timeout,
+    providers::info::{DefaultInfoProvider, InfoProvider},
+    routes::transactions::models::{
+        filters::{QueryParam, TransferFilters},
+        summary::TransactionListItem,
+    },
     utils::{context::RequestContext, errors::ApiResult},
 };
 
@@ -11,9 +20,39 @@ pub async fn get_incoming_transfers(
     cursor: &Option<String>,
     filters: &TransferFilters,
 ) -> ApiResult<Page<TransactionListItem>> {
+    let info_provider = DefaultInfoProvider::new(chain_id, context);
+    let incoming_page_metadata =
+        PageMetadata::from_cursor(cursor.as_ref().unwrap_or(&"".to_string()));
+
     return Ok(Page {
         next: None,
         previous: None,
         results: vec![],
     });
+}
+
+async fn fetch_backend_paged_txs(
+    context: &RequestContext,
+    info_provider: &impl InfoProvider,
+    safe_address: &str,
+    cursor: &Option<String>,
+    filters: &impl QueryParam,
+) -> ApiResult<Page<Transaction>> {
+    let other_filters = filters.as_query_param();
+    let page_metadata = PageMetadata::from_cursor(cursor.as_ref().unwrap_or(&"".to_string()));
+    let url = core_uri!(
+        info_provider,
+        "/v1/safes/{}/incoming-transfers/?{}&{}",
+        safe_address,
+        page_metadata.to_url_string(),
+        other_filters
+    )?;
+    log::debug!("request URL: {}", &url);
+    log::debug!("cursor: {:#?}", &cursor);
+    log::debug!("page_metadata: {:#?}", &page_metadata);
+    let body = RequestCached::new_from_context(url, context)
+        .request_timeout(transaction_request_timeout())
+        .execute()
+        .await?;
+    Ok(serde_json::from_str::<Page<Transaction>>(&body)?)
 }
--- src/routes/transactions/models/filters.rs
+++ src/routes/transactions/models/filters.rs
@@ -22,3 +22,31 @@ pub struct MultisigFilters {
     pub value: Option<String>,
     pub nonce: Option<String>,
 }
+
+pub trait QueryParam {
+    fn as_query_param(&self) -> String;
+}
+
+impl QueryParam for TransferFilters {
+    fn as_query_param(&self) -> String {
+        let mut query_params = String::new();
+
+        if let Some(date) = &self.date {
+            query_params.push_str(&format!("date={}&", date))
+        }
+
+        if let Some(to) = &self.to {
+            query_params.push_str(&format!("to={}&", to))
+        }
+
+        if let Some(value) = &self.value {
+            query_params.push_str(&format!("value={}&", value))
+        }
+
+        if let Some(token_address) = &self.token_address {
+            query_params.push_str(&format!("token_address={}&", token_address))
+        }
+
+        return query_params;
+    }
+}

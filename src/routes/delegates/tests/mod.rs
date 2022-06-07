4c26ddcc3cb2c7972de4940f76ae9219b238766e
moisses89 <moisses89.fn@gmail.com>
2022-06-07 10:28:37 +0200

    Add test for get delegates

--- src/routes/delegates/tests/mod.rs
+++ src/routes/delegates/tests/mod.rs
@@ -1,4 +1,6 @@
 mod routes;
 
-pub const BACKEND_LIST_DELEGATES_OF_SAFE: &str = include_str!("json/backend_list_delegates_of_safe.json");
-pub const EXPECTED_LIST_DELEGATES_OF_SAFE: &str = include_str!("json/expected_list_delegates_of_safe.json");
+pub const BACKEND_LIST_DELEGATES_OF_SAFE: &str =
+    include_str!("json/backend_list_delegates_of_safe.json");
+pub const EXPECTED_LIST_DELEGATES_OF_SAFE: &str =
+    include_str!("json/expected_list_delegates_of_safe.json");
--- src/routes/delegates/tests/routes.rs
+++ src/routes/delegates/tests/routes.rs
@@ -1,13 +1,15 @@
-use core::time::Duration;
-use mockall::predicate::eq;
 use crate::common::models::page::Page;
-use crate::routes::delegates::models::{Delegate, DelegateCreate, DelegateDelete, SafeDelegateDelete};
-use rocket::http::{ContentType, Header, Status};
-use rocket::local::asynchronous::Client;
+use crate::routes::delegates::models::{
+    Delegate, DelegateCreate, DelegateDelete, SafeDelegateDelete,
+};
 use crate::tests::main::setup_rocket;
 use crate::utils::http_client::{MockHttpClient, Request, Response};
+use core::time::Duration;
+use mockall::predicate::eq;
+use rocket::http::{ContentType, Header, Status};
+use rocket::local::asynchronous::Client;
 
-use crate::config::{default_request_timeout, chain_info_request_timeout};
+use crate::config::{chain_info_request_timeout, default_request_timeout};
 
 #[rocket::async_test]
 async fn get_delegates_from_safe() {
@@ -28,45 +30,42 @@ async fn get_delegates_from_safe() {
                 body: String::from(crate::tests::json::CHAIN_INFO_RINKEBY),
             })
         });
-    //Mocking response of transaction service delegates
+    // Mocking response of transaction service delegates
     let mut delegates_request = Request::new(format!(
         "https://safe-transaction.rinkeby.staging.gnosisdev.com/api/v1/delegates/?safe={}&delegate=&delegator=&label=",
         &safe_address));
     delegates_request.timeout(Duration::from_millis(default_request_timeout()));
-
-   
     mock_http_client
-    .expect_get()
-    .times(1)
-    .with(eq(delegates_request))
-    .returning(move |_| {
-        Ok(Response {
-            body: String::from(super::BACKEND_LIST_DELEGATES_OF_SAFE),
-            status_code: 200,
-        })
-    });
-    
+        .expect_get()
+        .times(1)
+        .with(eq(delegates_request))
+        .returning(move |_| {
+            Ok(Response {
+                body: String::from(super::BACKEND_LIST_DELEGATES_OF_SAFE),
+                status_code: 200,
+            })
+        });
     // setup route with mocked data
     let client = Client::tracked(
-        setup_rocket(mock_http_client, routes![super::super::routes::get_delegates]).await,
+        setup_rocket(
+            mock_http_client,
+            routes![super::super::routes::get_delegates],
+        )
+        .await,
     )
     .await
     .expect("valid rocket instance");
-
     let expected =
-        super::EXPECTED_LIST_DELEGATES_OF_SAFE;
-    
+        serde_json::from_str::<Page<Delegate>>(super::EXPECTED_LIST_DELEGATES_OF_SAFE).unwrap();
     // Requesting delegates to client-gateway
     let request = client
         .get(format!("/v1/chains/{}/delegates?safe={}", 4, &safe_address))
         .header(Header::new("Host", "test.gnosis.io/api"))
         .header(ContentType::JSON);
-    
-    let response = request.dispatch().await; 
+    let response = request.dispatch().await;
     let actual_status = response.status();
     let actual_json_body = response.into_string().await.unwrap();
-
-    
+    let actual = serde_json::from_str::<Page<Delegate>>(&actual_json_body).unwrap();
     assert_eq!(actual_status, Status::Ok);
-    //assert_eq!(actual_json_body, expected);
+    assert_eq!(actual, expected);
 }

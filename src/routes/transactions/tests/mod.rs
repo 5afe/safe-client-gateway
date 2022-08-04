758ea87b46a049ce810e999fca8030f8069655a5
fmrsabino <3332770+fmrsabino@users.noreply.github.com>
2022-08-04 17:40:28 +0200

    Add test cases to preview route

--- src/routes/transactions/tests/mod.rs
+++ src/routes/transactions/tests/mod.rs
@@ -5,3 +5,7 @@ const POST_CONFIRMATION_RESULT: &str = include_str!("json/post_confirmation_resu
 const MULTISIG_TX_DETAILS: &str = include_str!("json/multisig_tx_details.json");
 const CONTRACTS_RESPONSE: &str = include_str!("json/contracts_response.json");
 const PREVIEW_RESPONSE: &str = include_str!("json/preview_response.json");
+const PREVIEW_DATA_DECODED_ERROR_RESPONSE: &str =
+    include_str!("json/preview_response_data_decoded_error.json");
+const CHAIN_RESPONSE: &str = include_str!("json/chain_response.json");
+const CONTRACT_INFO: &str = include_str!("json/contract_info_BID.json");

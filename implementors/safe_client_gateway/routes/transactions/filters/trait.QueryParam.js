700c687e5ac38bb607132bc209f2110098e2d0f1
fmrsabino <fmrsabino@users.noreply.github.com>
2022-11-21 09:33:14 +0000

    deploy: c2a6fb35305aeac4119eddf5c0a149944fb64694

--- implementors/safe_client_gateway/routes/transactions/filters/trait.QueryParam.js
+++ implementors/safe_client_gateway/routes/transactions/filters/trait.QueryParam.js
@@ -1,3 +1,3 @@
-(function() {var implementors = {};
-implementors["safe_client_gateway"] = [];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
+(function() {var implementors = {
+"safe_client_gateway":[]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()

9ef0b54119b7c7b75d873b667552c56269155780
Fernando Paredes Garcia <fernando@develcuy.com>
2023-05-18 20:13:06 -0500

    Use safe-global branding.

--- docker-build-dev.sh
+++ docker-build-dev.sh
@@ -2,7 +2,7 @@
 
 # Developer friendly (faster build)
 
-DOCKERHUB_ORG=syscoin
+DOCKERHUB_ORG=safe-global
 DOCKERHUB_PROJECT=safe-client-gateway
 
 docker build -t $DOCKERHUB_ORG/$DOCKERHUB_PROJECT:dev -f Dockerfile.dev .

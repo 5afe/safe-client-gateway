0112226a80adf014702e566d6a55111634a5fa90
Fernando Paredes Garcia <fernando@develcuy.com>
2023-05-18 20:16:23 -0500

    Use safe-global branding.

--- docker-build.sh
+++ docker-build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export GITHUB_RUN_NUMBER=1
-export DOCKERHUB_ORG=syscoin
+export DOCKERHUB_ORG=safe-global
 export DOCKERHUB_PROJECT=safe-client-gateway
 export TARGETPLATFORM=linux/amd64
 ./scripts/build_docker.sh latest
